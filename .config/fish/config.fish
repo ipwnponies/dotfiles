ff856a44baeab1c74ed3bdf3c7d984da0cac7855
John Nguyen <jngu@yelp.com>
2017-03-04 18:25:30 -0800

    Fix fish prompt to match bash

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,5 @@
 set -x PATH ~/bin ~/.local/bin $PATH
 set -x EDITOR vim
+
+# Show entire dir name for pwd
+set -x fish_prompt_pwd_dir_length 0
--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -43,10 +43,39 @@ if not set -q __fish_prompt_cwd
             set -g __fish_prompt_cwd (set_color $fish_color_cwd)
         end
 
-printf '[%s] %s%s@%s %s%s %s%s \f\r> ' (date "+%H:%M:%S") "$__fish_color_blue" $USER $__fish_prompt_hostname "$__fish_prompt_cwd" "$PWD" "$stat" "$__fish_prompt_normal"
+printf '╭─[%s] %s%s%s@%s%s%s: %s%s %s(%s)%s%s \f\r╰─➤ ' (date "+%Y-%m-%d %H:%M:%S") "$__fish_color_blue" $USER $__fish_prompt_normal (set_color -o green) $__fish_prompt_hostname $__fish_prompt_normal (set_color -o yellow) (prompt_pwd $PWD) (set_color -o cyan) (__fish_prompt_git) "$stat" "$__fish_prompt_normal"
 
 # Keep tmux ssh-agent information up-to-date
 eval export "(tmux show-environment | grep \^SSH_AUTH_SOCK=)"
 
 end
+end
+
+function __fish_prompt_git
+    set git_root_dir (git rev-parse --git-dir 2>/dev/null)
+    if [ -n $git_root_dir ];
+        if [ -d "$git_root_dir/../.dotest" ];
+            set r "|AM/REBASE"
+            set b (git symbolic-ref HEAD 2>/dev/null)
+        else if [ -f "$git_root_dir/.dotest-merge/interactive" ];
+            set r "|REBASE-i"
+            set b (cat $git_root_dir/.dotest-merge/head-name)
+        else if [ -d "$git_root_dir/.dotest-merge" ];
+            set r "|REBASE-m"
+            set b (cat $git_root_dir/.dotest-merge/head-name)
+        else if [ -f "$git_root_dir/MERGE_HEAD" ];
+            set r "|MERGING"
+            set b (git symbolic-ref HEAD 2>/dev/null)
+        else;
+            if [ -f $git_root_dir/BISECT_LOG ];
+                set r "|BISECTING"
+            end
+            set b (git symbolic-ref HEAD 2>/dev/null)
+            if [ ! $status ];
+                set b (cut -c1-7 $git_root_dir/HEAD)...
+            end
+        end
+
+        printf "%s%s" (string replace "refs/heads/" "" "$b") "$r"
+    end
 end
