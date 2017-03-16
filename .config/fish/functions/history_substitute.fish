b8d9e52f8bcc106191043ea1d1c2a02cd3b8e003
John Nguyen <ipwnponies@gmail.com>
2017-03-15 17:34:41 -0700

    Add history_substitution proxy to fish

---
+++ .config/fish/functions/history_substitute.fish
@@ -0,0 +1,22 @@
+function history_substitute --description "Load up recent command history into EDITOR. This is the poor man's history substitution."
+
+    if date --version ^ /dev/null
+        # Return all commands used in last 20 mins
+        set -l start_time (date -d '20 min ago' +%s)
+        set history_lines (history --max 10 --show-time='%s ' | awk '$1 > $start_time')
+    else
+        # BSD date does not support --version. It also doesn't do relative dates.
+        # Return last ten lines
+        set history_lines (history --max 10)
+    end
+    
+    # Replace current buffer with history
+    commandline -r $history_lines
+    edit_command_buffer
+
+    if test (commandline -b | wc -l) -gt 2
+        # If there are more than 2 lines, we probably aborted history substitution
+        commandline -r ''
+    end
+end
+
