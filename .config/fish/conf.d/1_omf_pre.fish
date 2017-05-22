1049f386bc7ea181af52148038f794b62f8419e3
John Nguyen <ipwnponies@gmail.com>
2017-05-22 15:57:21 -0700

    Fix omf installation bootstrap syntax
    Fish doesn't support a variable in a command so an eval is necessary.

--- .config/fish/conf.d/1_omf_pre.fish
+++ .config/fish/conf.d/1_omf_pre.fish
@@ -2,5 +2,5 @@
 set -x Z_SCRIPT_PATH $XDG_CONFIG_HOME/z/z/z.sh
 
 if status --is-interactive; and not test -e $XDG_CONFIG_HOME/fish/conf.d/omf.fish
-    $XDG_CONFIG_HOME/fish/plugins/oh-my-fish/bin/install --noninteractive --offline
+    eval $XDG_CONFIG_HOME/fish/plugins/oh-my-fish/bin/install --noninteractive --offline
 end
