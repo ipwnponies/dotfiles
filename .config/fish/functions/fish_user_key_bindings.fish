3f9d81e1734591828fffc49af8ffdb36665b828f
John Nguyen <jngu@yelp.com>
2017-05-26 17:02:33 -0700

    Override omf-expand to restore original fish tab complete
    omf expand uses expand:choose-next function to initiate tab completion
    but it doesn't automatically use tab to start selection.

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,8 @@
 function fish_user_key_bindings
     bind \cx edit_command_buffer
+
+    # Override plugin-expand. Its expand:choose-next function has weird behaviour
+    bind -M expand \t complete
 end
 
 
