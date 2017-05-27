3dbc885d3f7a2f0c49720d77ce7f57df86094781
John Nguyen <jngu@yelp.com>
2017-05-26 17:08:27 -0700

    Differentiate between local and remote branch for git co completion
    I can't believe that fish git completion doesn't for git branch doesn't
    tell the difference between local and remote branches.

--- .config/fish/functions/git.fish
+++ .config/fish/functions/git.fish
@@ -1,7 +1,13 @@
-# These completions must be autoloaded because the command is autoloaded. Otherwise fish will fail
-# to find git and won't load this completion
-complete -f -c git -n '__fish_git_using_command co' -a '(__fish_git_branches)' --description 'Remote repo'
+# These completions must be autoloaded because the git function is autoloaded. Otherwise fish will fail
+# to find the command and get to loading this completion.
+complete -f -c git -n '__fish_git_using_command co' -a '(__fish_git_unique_remote_branches)' --description 'Remote Branch'
+complete -f -c git -n '__fish_git_using_command co' -a '(__fish_git_local_branches)' --description 'Local Branch'
+function __fish_git_local_branches
+    # Taken from fish shell git completions but changed git branch to only return local branches
+    command git branch --no-color $argv ^/dev/null | string match -v '\* (*)' | string match -r -v ' -> ' | string trim -c "* " | string replace -r "^remotes/" ""
+end
 
-if test -e git_local.fish
-    source git_local.fish
+set local_functions (dirname (status --current-filename))/git_local.fish
+if test -e $local_functions
+    source $local_functions
 end
