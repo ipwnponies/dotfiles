fc1a248d87c8ad4b9277171a10504b92ffdc3974
John Nguyen <jngu@yelp.com>
2017-06-07 15:29:29 -0700

    Add fzf alias
    This alias limits the size of fzf so it doesn't take over the terminal.
    Also fixes fzf key bindings for file search, command history search, and cd.

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -3,7 +3,6 @@ function fish_user_key_bindings
 
     # Override plugin-expand. Its expand:choose-next function has weird behaviour
     bind -M expand \t complete
+
+    fzf_key_bindings
 end
-
-
-fzf_key_bindings
