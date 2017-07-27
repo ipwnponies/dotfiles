69ecb131645035803a01d86625820f4b7f46c41c
John Nguyen <jngu@yelp.com>
2017-07-27 14:23:24 -0700

    [fish] Fix env startup (login vs interactive)

--- .config/fish/conf.d/000-env.fish
+++ .config/fish/conf.d/000-env.fish
@@ -1,18 +1,20 @@
-# XDG defaults
-set -x XDG_DATA_HOME $HOME/.local/share
-set -x XDG_CONFIG_HOME $HOME/.config
-set -x XDG_CACHE_HOME $HOME/.cache
+if status --is-login
+    # XDG defaults
+    set -x XDG_DATA_HOME $HOME/.local/share
+    set -x XDG_CONFIG_HOME $HOME/.config
+    set -x XDG_CACHE_HOME $HOME/.cache
 
-set PATH "$HOME/bin" $PATH
-set -x EDITOR vim
+    set PATH "$HOME/bin" $PATH
+    set -x EDITOR nvim
 
-# Show entire dir name for pwd
-set -x fish_prompt_pwd_dir_length 0
+    # Show entire dir name for pwd
+    set -x fish_prompt_pwd_dir_length 0
 
-if status --is-login; and status --is-interactive
-    bash -c 'source ~/.bash_profile' &
+    if status --is-interactive
+        bash -c 'source ~/.bash_profile' &
 
-    if not set -q TMUX; and set -q SSH_CLIENT; and type -q tmux
-        tmux attach
+        if not set -q TMUX; and set -q SSH_CLIENT; and type -q tmux
+            tmux attach
+        end
     end
 end
--- .config/fish/conf.d/fzf.fish
+++ .config/fish/conf.d/fzf.fish
@@ -1,5 +1,5 @@
 if status --is-login; and status --is-interactive
     if not test -d $XDG_CONFIG_HOME/fzf
-        vim -c 'silent! PlugInstall fzf' -c 'qa'
+        nvim -c 'silent! PlugInstall fzf' -c 'qa'
     end
 end
