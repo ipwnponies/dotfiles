46323b142dd6e3a68c7b05e0b5b8d2d19087b42b
John Nguyen <ipwnponies@gmail.com>
2017-09-01 09:33:41 -0700

    Fix up function to work with no arguments
    If `up` is called without arguments, this will implicitly be cd to
    parent directory.
    Fix bug with bad arguments, will no-op if `seq $levels` does not
    generate a list.

--- .config/fish/functions/up.fish
+++ .config/fish/functions/up.fish
@@ -1,7 +1,17 @@
 function up -d 'cd up directory with int param'
-    set levels (math $argv[1] + 0)
+    if test (count $argv) -eq 0
+        # Default to `up 1` if no arguments provided
+        set levels 1
+    else
+        # + 0 is used to cast to int
+        set levels (math $argv[1] + 0)
+    end
+
     for i in (seq $levels)
         set path "../$path"
     end
-    cd $path
+
+    if test -n $path
+        cd $path
+    end
 end
