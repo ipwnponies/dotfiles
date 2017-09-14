e5a42a59655ac5b0c0e21862aac169383c3c7b04
John Nguyen <ipwnponies@gmail.com>
2017-09-13 18:13:44 -0700

    [git/fish] Make git co use fzf
    This will use fzf to find local branches first, then remote branches.
    fzf is set to display last commit in preview window.

--- .config/fish/functions/git.fish
+++ .config/fish/functions/git.fish
@@ -1,10 +1,20 @@
-# These completions must be autoloaded because the git function is autoloaded. Otherwise fish will fail
-# to find the command and get to loading this completion.
-complete -f -c git -n '__fish_git_using_command co' -a '(__fish_git_unique_remote_branches)' --description 'Remote Branch'
-complete -f -c git -n '__fish_git_using_command co' -a '(__fish_git_local_branches)' --description 'Local Branch'
-function __fish_git_local_branches
+# These completions must go in functions, not completions.
+# They rely on git functions that must be autoloaded first.
+complete -r -f -c git -n '__fish_git_using_command co' -a '(__fish_git_co_branches)' --description 'Branches'
+function __fish_git_co_branches
+    set query (commandline -t)
+
     # Taken from fish shell git completions but changed git branch to only return local branches
-    command git branch --no-color $argv ^/dev/null | string match -v '\* (*)' | string match -r -v ' -> ' | string trim -c "* " | string replace -r "^remotes/" ""
+    command git branch --no-color -a ^/dev/null | \
+    string match -v '\* (*)' | \
+    string match -r -v ' -> ' | \
+    string trim -c "* " | \
+    string replace -r "^remotes/" "" | \
+    fzf --border --height 40% --min-height 10 --margin 1,5 --reverse --inline-info --preview 'git log -1 {}' --query $query
+
+    if test $status -ne 0
+        git symbolic-ref HEAD --short
+    end
 end
 
 set local_functions (dirname (status --current-filename))/git_local.fish
