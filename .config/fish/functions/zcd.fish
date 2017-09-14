a338ce0a6529aa0b9086ca6b0121f0c8e91b43b5
John Nguyen <ipwnponies@gmail.com>
2017-09-13 18:04:55 -0700

    [fish] Pass input to zcd as initial query

--- .config/fish/functions/zcd.fish
+++ .config/fish/functions/zcd.fish
@@ -1,6 +1,6 @@
 function zcd -d 'z autojump with fzf search interface'
     set -l Z_SCRIPT_PATH $OMF_PATH/pkg/z/z/z.sh
-    set directory (bash -c "source $Z_SCRIPT_PATH; _z" ^| awk '{print $2}' | fzf --border --height 40% --min-height 10 --margin 1,5 --reverse +s --tac)
+    set directory (bash -c "source $Z_SCRIPT_PATH; _z" ^| awk '{print $2}' | fzf --border --height 40% --min-height 10 --margin 1,5 --reverse +s --tac --query "$argv")
     if test $status -eq 0
         cd $directory
     end
