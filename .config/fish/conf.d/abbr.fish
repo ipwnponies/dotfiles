0cede379ea324c558379a4afc7eeb8717af6ee5c
John Nguyen <ipwnponies@gmail.com>
2017-09-13 18:06:13 -0700

    [fish] Add misc abbr
    - Fish wraps grep with colours, this makes it funky (ctrl-c twice).
    - ps is useful for looking at PID, so we don't want to set user.
    - fzf options to automatically quit/select the only choice
    - netstat to add common args

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -1,17 +1,18 @@
 # Abbreviations
-abbr --add g        'grep -Pi --color=auto'
+abbr --add g        'command grep -Pi --color=auto'
 abbr --add df       'df -h'
 abbr --add du       'du -h --max-depth=1'
 abbr --add j        'jobs'
 abbr --add g        'grep -Pi --color=auto'
-abbr --add ps       'ps -fu'
+abbr --add ps       'ps -f'
 abbr --add pgrep    'pgrep -fau (whoami)'
 abbr --add pkill    'pkill --fu (whoami)'
 abbr --add kill     'kill -s TERM'
 abbr --add fzfdo    'fzf | read -l fzf; and'
-abbr --add fzf      'fzf --border --height 40% --min-height 10 --margin 1,5 --reverse'
+abbr --add fzf      'fzf --border --height 40% --min-height 10 --margin 1,5 --reverse --inline-info -1 -0'
 abbr --add pytest   'pytest --verbose --capture no --pdbcls IPython.terminal.debugger:TerminalPdb'
 abbr --add less     'less -iR'
+abbr --add netstat  'netstat --listening --program --numeric'
 
 set local_functions (dirname (status --current-filename))/abbr_local.fish
 if test -e $local_functions
