b3f0c47c0ee9524b2bd66135c404ab7d56ceb142
John Nguyen <ipwnponies@gmail.com>
2017-09-27 10:07:56 -0700

    [venv] Install requirements-dev.txt in venv

--- .config/fish/conf.d/001_virtualenv.fish
+++ .config/fish/conf.d/001_virtualenv.fish
@@ -5,13 +5,14 @@ if status --is-login; and status --is-interactive; and type -q virtualenv;
     set venv "$XDG_DATA_HOME/virtualenv"
     set venv_update "$XDG_CONFIG_HOME/venv-update/venv-update"
     set requirements "$XDG_CONFIG_HOME/venv-update/requirements.txt"
+    set requirements_dev "$XDG_CONFIG_HOME/venv-update/requirements-dev.txt"
     set logfile "$XDG_CACHE_HOME/venv-update/log"
 
     set install_script "
         echo 'venv-updating...'
 
         mkdir -p (dirname $logfile)
-        if not $venv_update venv= -p python3 $venv install= -r $requirements >> $logfile
+        if not $venv_update venv= -p python3 $venv install= -r $requirements -r $requirements_dev >> $logfile
             echo 'Uh... that didn\'t work gud. So check out $logfile'
         end
         "
