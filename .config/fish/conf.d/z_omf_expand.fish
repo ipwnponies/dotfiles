79cd46bcf6c558507e0b9f270a3a62a1a028336e
John Nguyen <ipwnponies@gmail.com>
2017-09-27 09:15:46 -0700

    [fish] Simplify omf expand rules for history substitution
    By incorporating sed substitution with history and using the completion
    selection used by omf expand, we can reduce !! (last command), !-N (last
    Nth command), and s/*/* (sed substitute on last command) down to a
    single common command, s/*/* that provides sed completion suggestion for
    all recent history lines.

--- .config/fish/conf.d/z_omf_expand.fish
+++ .config/fish/conf.d/z_omf_expand.fish
@@ -4,13 +4,7 @@ if status --is-interactive; and set -q OMF_PATH
     expand-word -p '^!$' -e 'printf "%s\n" $history'
 
     # Bash's ^foo^bar^ substitution
-    expand-word -p '^s/..*/.*$' -e 'echo -n "$history[1]" | sed -e (commandline -t)/g'
-
-    # Bash's !! history substitution
-    expand-word -p '^!!$' -e 'echo -n $history[1]'
-
-    # Bash's !-n history substitution
-    expand-word -p '^!-[0-9]$' -e 'echo -n $history[(string sub -s -1 (commandline -t))]'
+    expand-word -p '^s/..*/.*$' -e 'printf "%s\n" $history | sed -e (commandline -t)/g'
 
     # Git merge expansion
     expand-word -p '^gitmer$' -e '_expand_gitmer'
