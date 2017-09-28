b7d43eb5431c7a7516ad614e265f5b9bdf37013c
John Nguyen <ipwnponies@gmail.com>
2017-09-28 09:44:40 -0700

    [fish] Have aactivator.fish check other candidate directories
    Typically python virtualenvs are created in virtualenv_run or venv, we
    should check if those exist and activate accordingly.

--- .config/fish/conf.d/aactivator.fish
+++ .config/fish/conf.d/aactivator.fish
@@ -1,23 +1,32 @@
 function _activate_venv --on-variable PWD --description 'Automatically activate venv upon changing directory'
     set -l path $PWD
+    set -l possible_venvs '/.activate.fish' '/virtualenv_run/bin/activate.fish' '/venv/bin/activate.fish'
 
     # Searches for aactivator bottom up
     while test $path != '/'
-        if test -e $path/.activate.fish
-            set activate_script_dir $path
+        for i in $path$possible_venvs
+            # Iterate through possible known venv directories
+            if test -e $i
+                set activate_script $i
+                break
+            end
+        end
+
+        # If venv found, exit loop, else go up a directory and continue
+        if set -q activate_script
             break
         else
             set path (dirname $path)
         end
     end
 
-    if set -q activate_script_dir
-        if set -q ACTIVATED_VENV; and test $activate_script_dir != $ACTIVATED_VENV;
+    if set -q activate_script
+        if set -q ACTIVATED_VENV; and test $activate_script != $ACTIVATED_VENV;
             deactivate
         end
 
-        set -g ACTIVATED_VENV $activate_script_dir
-        source $activate_script_dir/.activate.fish
+        set -g ACTIVATED_VENV $activate_script
+        source $activate_script
     else if set -q ACTIVATED_VENV
         # No aactivator found, deactivate current venv
         deactivate
