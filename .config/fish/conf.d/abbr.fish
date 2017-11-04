a22f99c7944eb4803bccfeaa7256b420cdb6ec1e
John Nguyen <ipwnponies@gmail.com>
2017-11-03 18:00:10 -0700

    [fish] More abbr

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -6,13 +6,14 @@ abbr --add j        'jobs'
 abbr --add g        'grep -Pi --color=auto'
 abbr --add ps       'ps -f'
 abbr --add pgrep    'pgrep -fau (whoami)'
-abbr --add pkill    'pkill --fu (whoami)'
+abbr --add pkill    'pkill -fu (whoami)'
 abbr --add kill     'kill -s TERM'
 abbr --add fzfdo    'fzf | read -l fzf; and'
 abbr --add fzf      'fzf --border --height 40% --min-height 10 --margin 1,5 --reverse --inline-info -1 -0 --bind change:top'
 abbr --add pytest   'pytest --verbose --capture no --pdbcls IPython.terminal.debugger:TerminalPdb'
 abbr --add less     'less -iR'
 abbr --add netstat  'netstat --listening --program --numeric'
+abbr --add jq       'jq -C -S'
 
 set local_functions (dirname (status --current-filename))/abbr_local.fish
 if test -e $local_functions
