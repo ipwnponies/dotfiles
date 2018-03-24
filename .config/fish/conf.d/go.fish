55a3bc348fbdffdadbbd6a51d9c6be9ee88b8193
John Nguyen <ipwnponies@gmail.com>
2018-03-24 16:37:07 -0700

    [go] Install gron
    gron is used to denomalize json output. This makes it highly greppable
    and manipulable as plaintext, without needing a json parser.

--- .config/fish/conf.d/go.fish
+++ .config/fish/conf.d/go.fish
@@ -2,4 +2,5 @@ if status --is-login; and status --is-interactive; and type -q go
     set -gx GOPATH $XDG_DATA_HOME/go
     set PATH $PATH $GOPATH/bin
     go get github.com/mkchoi212/fac
+    go get github.com/tomnomnom/gron
 end
