c723ebb7c284f6eef4a18d307ab13b5ca46fe73c
John Nguyen <ipwnponies@gmail.com>
2018-04-04 10:39:15 -0700

    [vim] Fix vim-plug install to be quiet
    Vim doesn't like being piped to non-tty and prints a warning when we try
    to pipe to /dev/null. The more correct way to swallow outputs is to use
    silent argument (-s) which only applies in ex mode (-e).

--- .config/fish/conf.d/vim-plug.fish
+++ .config/fish/conf.d/vim-plug.fish
@@ -1,5 +1,5 @@
 # Installation of vim-plug plugins
 
 if status --is-login; and status --is-interactive;
-    eval $EDITOR -c PlugInstall -c qa >/dev/null
+    eval $EDITOR -e -s -c PlugInstall -c qa
 end
