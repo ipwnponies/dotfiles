86e170d3535c5fbf0ae5dee2bf0c94bad2a03d4c
John Nguyen <ipwnponies@gmail.com>
2018-04-17 14:50:47 -0700

    [go] Install jiq via go get
    Switch from downloading jiq binary to installing via go get. This is
    because the gobuilder.me services is defunct.

--- .config/fish/conf.d/go.fish
+++ .config/fish/conf.d/go.fish
@@ -1,6 +1,8 @@
 if status --is-login; and status --is-interactive; and type -q go
     set -gx GOPATH $XDG_DATA_HOME/go
     set PATH $PATH $GOPATH/bin
+
+    go get github.com/fiatjaf/jiq/cmd/jiq
     go get github.com/mkchoi212/fac
     go get github.com/tomnomnom/gron
 end
