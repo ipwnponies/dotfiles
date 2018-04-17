55ad26b423135e7007f5ca199e588733d50f36ba
John Nguyen <ipwnponies@gmail.com>
2018-04-17 12:43:18 -0700

    [venv] Pin requirements for bootstrapping
    Pin requirements for bootstrapping so we can reliably reproduce dev
    environment and bump the packages at our own pace.
    Fix error message to print out log filepath.

--- .config/fish/conf.d/001_virtualenv.fish
+++ .config/fish/conf.d/001_virtualenv.fish
@@ -6,13 +6,18 @@ if status --is-login; and status --is-interactive; and type -q virtualenv;
     set venv_update "$XDG_CONFIG_HOME/venv-update/venv-update"
     set requirements "$XDG_CONFIG_HOME/venv-update/requirements.txt"
     set requirements_dev "$XDG_CONFIG_HOME/venv-update/requirements-dev.txt"
+    set requirements_bootstrap "$XDG_CONFIG_HOME/venv-update/requirements-bootstrap.txt"
     set logfile "$XDG_CACHE_HOME/venv-update/log"
 
     echo 'venv-updating...'
 
     mkdir -p (dirname $logfile)
-    if not eval $venv_update venv= -p python3 $venv install= -r $requirements -r $requirements_dev >> $logfile
-        echo 'Uh... that didn\'t work gud. So check out $logfile'
+    if not eval $venv_update \
+        venv= -p python3 $venv \
+        bootstrap-deps= -r $requirements_bootstrap \
+        install= -r $requirements -r $requirements_dev \
+        >> $logfile
+        echo "Uh... that didn\'t work gud. So check out $logfile"
     end
 
     set PATH "$venv/bin" $PATH
