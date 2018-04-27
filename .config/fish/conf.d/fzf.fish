b07e4357cdb1713386531a3fbed92cbd22814dab
John Nguyen <ipwnponies@gmail.com>
2018-04-26 17:35:09 -0700

    [fzf] Add fzf key binding for toggle sort
    Often times, it's easier to search by sorted entries if you know what
    you're looking for and you're just lazy.
    Other times, you want to keep the order stable, to encourage shallower
    results or results that appear earlier.

--- .config/fish/conf.d/fzf.fish
+++ .config/fish/conf.d/fzf.fish
@@ -1,5 +1,7 @@
 if status --is-login; and status --is-interactive
     if type -q fzf
-        set -gx FZF_DEFAULT_OPTS '--border --height 40% --min-height 10 --margin 1,5 --reverse -1 -0 --inline-info --bind pgdn:preview-page-down,pgup:preview-page-up,change:top'
+        set -gx FZF_DEFAULT_OPTS "--reverse --select-1 --exit-0 \
+        --border --height 40% --min-height 10 --margin 1,5 --inline-info \
+        --bind pgdn:preview-page-down,pgup:preview-page-up,change:top,ctrl-s:toggle-sort"
     end
 end
