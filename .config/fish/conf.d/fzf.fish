5eef103e6071bf2624fc70be5cef0676518b037c
John Nguyen <ipwnponies@gmail.com>
2018-07-03 13:10:40 -0700

    [fzf] Set default behaviour to no sorting
    Default to no sorting. This seems to be the most common use case. And
    where we do intend to sort, it seems that we're piping to `sort` by
    convention anyways.
    Preserving the input order lets this behaviour more intuitively.
    There exists the binding for toggling sorting.

--- .config/fish/conf.d/fzf.fish
+++ .config/fish/conf.d/fzf.fish
@@ -1,6 +1,6 @@
 if status --is-login; and status --is-interactive
     if type -q fzf
-        set -gx FZF_DEFAULT_OPTS "--reverse --select-1 --exit-0 \
+        set -gx FZF_DEFAULT_OPTS "--reverse --select-1 --exit-0 --no-sort \
         --border --height 40% --min-height 10 --margin 1,5 --inline-info \
         --bind pgdn:preview-page-down,pgup:preview-page-up,change:top,ctrl-s:toggle-sort"
     end
