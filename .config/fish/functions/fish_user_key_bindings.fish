49f74c7441d7d3c0ba710d5c4669974d81566e1e
John Nguyen <ipwnponies@gmail.com>
2018-07-03 13:14:43 -0700

    [fish] Remove unused binding for editing commandline
    Fish has a builtin binding already for this: <m-v>

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,4 @@
 function fish_user_key_bindings
-    bind \cx edit_command_buffer
-
     # Override plugin-expand. Its expand:choose-next function has weird behaviour
     bind -M expand \t complete
 
