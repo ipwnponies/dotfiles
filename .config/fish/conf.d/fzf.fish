02d8fd8b6e6c50266bad160045fab10f4485cac9
John Nguyen <ipwnponies@gmail.com>
2018-07-05 15:54:04 -0700

    Revert "[fzf] Set default behaviour to no sorting"
    This reverts commit 5eef103e6071bf2624fc70be5cef0676518b037c.
    fzf does not have way to toggle sorting. We'll have to default to
    sorting and selectively disable based on commands.

--- .config/fish/conf.d/fzf.fish
+++ .config/fish/conf.d/fzf.fish
@@ -1,6 +1,6 @@
 if status --is-login; and status --is-interactive
     if type -q fzf
-        set -gx FZF_DEFAULT_OPTS "--reverse --select-1 --exit-0 --no-sort \
+        set -gx FZF_DEFAULT_OPTS "--reverse --select-1 --exit-0 \
         --border --height 40% --min-height 10 --margin 1,5 --inline-info \
         --bind pgdn:preview-page-down,pgup:preview-page-up,change:top,ctrl-s:toggle-sort"
     end
