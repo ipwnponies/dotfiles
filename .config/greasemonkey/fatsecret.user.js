acb23904389fd1252e2cf88740917e330677d8b4
John Nguyen <ipwnponies@gmail.com>
2018-09-08 21:03:54 -0700

    [greasemonkey] Fix bug with fatsecret script

--- .config/greasemonkey/fatsecret.user.js
+++ .config/greasemonkey/fatsecret.user.js
@@ -2,12 +2,17 @@
 // @name        FatSecret redirect to absolute date
 // @namespace   ipwnponies
 // @description Resolves food diary to absolute url. Because they try to do clever, stateful shit using with cookies.
-// @version     1.0
-// @match       https:*.fatsecret.com/Diary.aspx?pa=fj
+// @version     1.0.1
+// @match       https://*.fatsecret.com/Diary.aspx
 // @grant       none
 // ==/UserScript==
 
-(() => {
+const isDateQueryParamMissing = () => {
+  const params = (new URL(document.location)).searchParams;
+  return params.toString() == 'pa=fj';
+};
+
+const main = () => {
   const currentDate = new Date();
 
   // Why the shit is in this in minutes. I get it, there's places in the world with 30 minute offset.
@@ -16,5 +21,16 @@
   const local_time = currentDate.getTime() - timezone_offset;
   const days_epoch = Math.floor(local_time / (1000*60*60*24));
 
-  window.location.replace('?pa=fj&dt=' + days_epoch);
-})()
+  const newParams = new URLSearchParams();
+  newParams.set('pa', 'fj');
+  newParams.set('dt', days_epoch);
+
+  const redirectUrl = new URL(document.location);
+  redirectUrl.search = newParams;
+
+  document.location.replace(redirectUrl);
+};
+
+if (isDateQueryParamMissing()) {
+  main();
+}
