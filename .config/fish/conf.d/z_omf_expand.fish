6b5aa8c164764e45af04b9eee17f5a0ea0ad42e8
John Nguyen <ipwnponies@gmail.com>
2018-09-24 19:22:53 -0700

    [fish] Support sed syntax with alternative delimiter
    This is janky but works "good enough". Let's you use any delimiter, like
    sed does.

--- .config/fish/conf.d/z_omf_expand.fish
+++ .config/fish/conf.d/z_omf_expand.fish
@@ -3,13 +3,18 @@
 if status --is-interactive; and set -q OMF_PATH
     expand-word -p '^!$' -e 'printf "%s\n" $history'
 
-    # Bash's ^foo^bar^ substitution
-    expand-word -p '^s/..*/.*$' -e '_expand_sed_history'
+    # Bash's ^foo^bar^ substitution. Supports sed syntax command
+    expand-word -p '^s(.)..*\1.*$' -e '_expand_sed_history'
     function _expand_sed_history
-		set sed_command (commandline -t)
-		set search_term (string split '/' $sed_command )[2]
-		printf '%s\n' $history | grep $search_term | sed -e "$sed_command/g"
-	end
+        set sed_command (commandline -t)
+        set delimiter (string sub --start 2 --length 1 $sed_command)
+        set search_term (string split $delimiter $sed_command)[2]
+
+        # Close sed command and add default option, global
+        test (count (string match --all $delimiter $sed_command) -gt 2); and set sed_command $sed_command$delimiter'g'
+
+        printf '%s\n' $history | grep $search_term | sed -e $sed_command
+    end
 
     # Git merge expansion
     expand-word -p '^gitmer$' -e '_expand_gitmer'
