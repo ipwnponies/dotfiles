d185239407025e9f9368c5b45c880f9b15dfa2ed
John Nguyen <ipwnponies@gmail.com>
2018-11-08 16:29:28 -0800

    [fish] Move omf plugin configurations to omf conf
    This will ensure the ordering and dependency is at least coupled more
    closely.
    diff --git a/.config/fish/conf.d/omf.fish b/.config/fish/conf.d/omf.fish
    index 4386a2b..13506ba 100644
    --- a/.config/fish/conf.d/omf.fish
    +++ b/.config/fish/conf.d/omf.fish
    @@ -4,9 +4,7 @@ if status --is-interactive;
         if test -d $omf_conf
             # Configure oh-my-fish
    
    -        set -q XDG_DATA_HOME;
    -            and set -gx OMF_PATH "$XDG_DATA_HOME/omf";
    -            or set -gx OMF_PATH "$HOME/.local/share/omf";
    +        set -gx OMF_PATH "$XDG_DATA_HOME/omf";
    
             # Load Oh My Fish configuration.
             source $OMF_PATH/init.fish
    diff --git a/.config/fish/conf.d/z_omf_expand.fish b/.config/fish/conf.d/z_omf_expand.fish
    deleted file mode 100644
    index 71b5b2f..0000000
    --- a/.config/fish/conf.d/z_omf_expand.fish
    +++ /dev/null
    @@ -1,27 +0,0 @@
    -# Configure plugin-expand
    -
    -if status --is-interactive; and set -q OMF_PATH
    -    expand-word -p '^!$' -e 'printf "%s\n" $history'
    -
    -    # Bash's ^foo^bar^ substitution. Supports sed syntax command
    -    expand-word -p '^s(.)..*\1.*$' -e '_expand_sed_history'
    -    function _expand_sed_history
    -        set sed_command (commandline -t)
    -        set delimiter (string sub --start 2 --length 1 $sed_command)
    -        set search_term (string split $delimiter $sed_command)[2]
    -
    -        # Close sed command and add default option, global
    -        test (count (string match --all $delimiter $sed_command) -gt 2); and set sed_command $sed_command$delimiter'g'
    -
    -        printf '%s\n' $history | grep $search_term | sed -e $sed_command
    -    end
    -
    -    # Git merge expansion
    -    expand-word -p '^gitmer$' -e '_expand_gitmer'
    -    function _expand_gitmer
    -        # Suggest previous branch as default first candidate, then the rest of existing branches
    -        set -l branches (git rev-parse --abbrev-ref '@{-1}') (git for-each-ref --format '%(refname:short)' refs/heads/)
    -        # Provide optional suggestion for --no-ff
    -        printf 'git merge %s\n' $branches{,' --no-ff'}
    -    end
    -end
    diff --git a/.config/omf/init.fish b/.config/omf/init.fish
    index 64fed5c..5ee9442 100644
    --- a/.config/omf/init.fish
    +++ b/.config/omf/init.fish
    @@ -1,2 +1,29 @@
      # Configure path to z (autojump)
     set -gx _Z_DATA $XDG_CACHE_HOME/z_datafile
    +
    +# plugin-expand
    +begin
    +    expand-word -p '^!$' -e 'printf "%s\n" $history'
    +
    +    # Bash's ^foo^bar^ substitution. Supports sed syntax command
    +    expand-word -p '^s(.)..*\1.*$' -e '_expand_sed_history'
    +    function _expand_sed_history
    +        set sed_command (commandline -t)
    +        set delimiter (string sub --start 2 --length 1 $sed_command)
    +        set search_term (string split $delimiter $sed_command)[2]
    +
    +        # Close sed command and add default option, global
    +        test (count (string match --all $delimiter $sed_command) -gt 2); and set sed_command $sed_command$delimiter'g'
    +
    +        history search $search_term | sed -e $sed_command
    +    end
    +
    +    # Git merge expansion
    +    expand-word -p '^gitmer$' -e '_expand_gitmer'
    +    function _expand_gitmer
    +        # Suggest previous branch as default first candidate, then the rest of existing branches
    +        set -l branches (git rev-parse --abbrev-ref '@{-1}') (git for-each-ref --format '%(refname:short)' refs/heads/)
    +        # Provide optional suggestion for --no-ff
    +        printf 'git merge %s\n' $branches{,' --no-ff'}
    +    end
    +end

--- .config/fish/conf.d/omf.fish
+++ .config/fish/conf.d/omf.fish
@@ -4,9 +4,7 @@ if status --is-interactive;
     if test -d $omf_conf
         # Configure oh-my-fish
 
-        set -q XDG_DATA_HOME;
-            and set -gx OMF_PATH "$XDG_DATA_HOME/omf";
-            or set -gx OMF_PATH "$HOME/.local/share/omf";
+        set -gx OMF_PATH "$XDG_DATA_HOME/omf";
 
         # Load Oh My Fish configuration.
         source $OMF_PATH/init.fish
