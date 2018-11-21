0fd7a0599ff3d0a87605d4f9a1f6ff027250baf7
John Nguyen <ipwnponies@gmail.com>
2018-11-20 18:01:17 -0800

    [fish] Add watch abbreviation
    Why watch doesn't have reasonable defaults, I don't know. At least I
    think they're reasonable:
    - colours
    - delta highlighting

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -9,12 +9,14 @@ abbr --add pgrep    'pgrep -fau (whoami)'
 abbr --add pkill    'pkill -fu (whoami)'
 abbr --add kill     'kill -s TERM'
 abbr --add fzfdo    'fzf | read -l fzf; and'
-set -l pytest_args     '-vv --capture no --exitfirst --failed-first tests/'
-abbr --add pytest   "pytest $pytest_args"
-abbr --add pytestw  "pytest-watch -- $pytest_args"
 abbr --add less     'less -iR'
 abbr --add netstat  'netstat --listening --program --numeric'
 abbr --add jq       'jq --color-output --sort-keys'
+abbr --add watch    'watch -cd -n 5'
+
+set -l pytest_args  '-vv --capture no --exitfirst --failed-first tests/'
+abbr --add pytest   "pytest $pytest_args"
+abbr --add pytestw  "pytest-watch -- $pytest_args"
 
 set -l local_functions (dirname (status --current-filename))/abbr_local.fish
 if test -e $local_functions
