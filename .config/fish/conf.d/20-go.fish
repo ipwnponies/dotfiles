554e1d8cd81c4138f450ce7770ba14b0cbb46ea3
John Nguyen <ipwnponies@gmail.com>
2019-01-03 18:34:36 -0800

    [go] Add dive
    dive is a tool for visualizing the layers of a docker image and
    estimating the compression efficiency.

--- .config/fish/conf.d/20-go.fish
+++ .config/fish/conf.d/20-go.fish
@@ -2,7 +2,8 @@ if status --is-login; and status --is-interactive; and type -q go
     set -gx GOPATH $XDG_DATA_HOME/go
     set PATH $PATH $GOPATH/bin
 
-    type -q jiq; or go get github.com/fiatjaf/jiq/cmd/jiq
-    type -q fac; or go get github.com/mkchoi212/fac
-    type -q gron; or go get github.com/tomnomnom/gron
+    type -q jiq; or begin; echo 'Installing jiq...'; and go get github.com/fiatjaf/jiq/cmd/jiq; end
+    type -q fac; or begin; echo 'Installing fac...'; and go get github.com/mkchoi212/fac; end
+    type -q gron; or begin; echo 'Installing gron...'; and go get github.com/tomnomnom/gron; end
+    type -q dive; or begin; echo 'Installing dive...'; and go get github.com/wagoodman/dive; end
 end
