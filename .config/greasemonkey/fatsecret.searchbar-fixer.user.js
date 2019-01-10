cd48b0e239ecb02eb0dcf056b7c7ae8da4b86234
John Nguyen <ipwnponies@gmail.com>
2019-01-10 01:22:31 -0800

    [greasemonkey] Enhance fatsecret greasemoneky script
    In addition to invoking mouse click events upon tab focus, this will
    also make the "Add Item" button work more intuitively. It will
    immediately focus the search bar and be ready to accept input.

--- .config/greasemonkey/fatsecret.searchbar-fixer.user.js
+++ .config/greasemonkey/fatsecret.searchbar-fixer.user.js
@@ -1,16 +1,25 @@
 // ==UserScript==
-// @name        Fix search bar behaviour
+// @name        Fix add item and search bar behaviour
 // @namespace   ipwnponies
 // @description Searching only works when input is clicked, which is not the only way a textbox can be used.
-// @version     1.0.0
+//              This script will correct "Add Item" button behaviour: upon click, immediately focus the search bar and
+//              also trigger any bespoke magicks. So that we end up with a normal-ass website.
+// @version     1.1.0
 // @match       https://www.fatsecret.com/Diary.aspx
 // @grant       none
 
-const fatsecret = () => {
-  for (var i of ['searchBfastExp', 'searchLunchExp', 'searchDinnerExp', 'searchOtherExp']) {
-    const input = document.getElementById(i);
-    input.addEventListener('focus', input.onclick);
-  };
+for (var i of ['addBfast', 'addLunch', 'addDinner', 'addOther']) {
+    const addItemButton = document.getElementById(i);
+    addItemButton.addEventListener('click', event=> {
+        // Search bar is not nested under "Add item", it's an invisible div at the same level. It doesn't make sense
+        const input = addItemButton.nextElementSibling.querySelector('input');
+
+        // Trigger click events, which is where auto suggestion logic is invoked
+        input.click();
+
+        // Focus the search bar so searching can begin immediately
+        input.focus();
+    });
 };
 
 fatsecret();
