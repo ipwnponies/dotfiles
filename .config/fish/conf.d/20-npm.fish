42200c0be7d9e07032c5b50e3eeb12a1666947ee
John Nguyen <ipwnponies@gmail.com>
2019-01-19 13:56:11 -0800

    [npm] Async npm install
    npm tools are not requisite of dotfiles bootstrap. They can be installed
    asynchronously, all that's needed is to add to PATH (static
    destination).

--- .config/fish/conf.d/20-npm.fish
+++ .config/fish/conf.d/20-npm.fish
@@ -4,11 +4,13 @@ if status --is-login; and status --is-interactive; and type -q npm;
 
     set venv "$XDG_CONFIG_HOME/npm"
     set logfile "$XDG_CACHE_HOME/npm"
-    mkdir -p (dirname $logfile)
 
-    if not npm install --loglevel=error --prefix $venv >> $logfile
-        echo 'Uh... that didn\'t work gud. So check out $logfile'
-    end
+    fish -c "
+        mkdir -p (dirname $logfile)
+        if not npm install --loglevel=error --prefix $venv >> $logfile
+            echo 'Uh... that didn\'t work gud. So check out $logfile'
+        end
+    " &
 
     set PATH "$venv/node_modules/.bin" $PATH
 end
