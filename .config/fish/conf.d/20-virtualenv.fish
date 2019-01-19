e2b500190902777db85a53438536c246df8a79f2
John Nguyen <ipwnponies@gmail.com>
2019-01-19 13:59:14 -0800

    [venv] Bootstrap asynchronously
    These tools are not needed for dotfiles bootstrap, just need to set the
    PATH (static destination).
    Remove unnecessary console noise.

--- .config/fish/conf.d/20-virtualenv.fish
+++ .config/fish/conf.d/20-virtualenv.fish
@@ -9,17 +9,19 @@ if status --is-login; and status --is-interactive; and type -q virtualenv;
     set requirements_bootstrap "$XDG_CONFIG_HOME/venv-update/requirements-bootstrap.txt"
     set logfile "$XDG_CACHE_HOME/venv-update/log"
 
-    echo 'venv-updating...'
 
-    mkdir -p (dirname $logfile)
-    if not fish -c "
-        $venv_update \
-            venv= -p python3 $venv \
-            bootstrap-deps= -r $requirements_bootstrap \
-            install= -r $requirements -r $requirements_dev \
-        " >> $logfile
-        echo "Uh... that didn\'t work gud. So check out $logfile"
-    end
+    # Run venv-update in background because most of the time, this is noop
+    # When it does change something, we only need the side effects (new programs installed)
+    fish -c "
+        mkdir -p (dirname $logfile)
+        if not $venv_update \
+                venv= -p python3 $venv \
+                bootstrap-deps= -r $requirements_bootstrap \
+                install= -r $requirements -r $requirements_dev \
+             >> $logfile
+            echo 'Uh... that didn\'t work gud. So check out $logfile'
+        end
+    " &
 
     set PATH "$venv/bin" $PATH
 end
