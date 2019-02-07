db82a190d11bd5ea27648610cfa29600b27a6818
John Nguyen <ipwnponies@gmail.com>
2019-02-07 01:19:52 -0800

    [greasemonkey] Revert fatsecret greasemonkey script to previous behaviour
    Turns out their site has more bad UX: when the search bar is focused, it
    shows search results for partial search text. Since the initial text is
    blank, that means it starts off showing blank results.
    
    This reverts to previous behaviour, which defaults to showing recent
    items.

--- .config/greasemonkey/fatsecret.searchbar-fixer.user.js
+++ .config/greasemonkey/fatsecret.searchbar-fixer.user.js
@@ -2,24 +2,15 @@
 // @name        Fix add item and search bar behaviour
 // @namespace   ipwnponies
 // @description Searching only works when input is clicked, which is not the only way a textbox can be used.
-//              This script will correct "Add Item" button behaviour: upon click, immediately focus the search bar and
-//              also trigger any bespoke magicks. So that we end up with a normal-ass website.
-// @version     1.1.0
+//              This script will correct "Add Item" button behaviour by triggering any bespoke magicks.
+//              So that we end up with a normal-ass website.
+// @version     1.2.0
 // @match       https://www.fatsecret.com/Diary.aspx
 // @grant       none
 
 for (var i of ['addBfast', 'addLunch', 'addDinner', 'addOther']) {
-    const addItemButton = document.getElementById(i);
-    addItemButton.addEventListener('click', event=> {
-        // Search bar is not nested under "Add item", it's an invisible div at the same level. It doesn't make sense
-        const input = addItemButton.nextElementSibling.querySelector('input');
-
-        // Trigger click events, which is where auto suggestion logic is invoked
-        input.click();
-
-        // Focus the search bar so searching can begin immediately
-        input.focus();
-    });
+  const search_input = document.querySelector(`#${i} + * input`);
+  search_input.addEventListener('focus', event => {
+    search_input.click();
+  });
 };
-
-fatsecret();
