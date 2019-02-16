bbf7f02536e774f6a7a6ad8c39cc929090f000e7
John Nguyen <ipwnponies@gmail.com>
2019-02-16 13:00:24 -0800

    [fish] Add abbreviation for time
    The default format for time is so hard to read. And I've decided that I
    really only care about using time for elapsed wall clock time.

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -1,21 +1,22 @@
 # Abbreviations
-abbr --add df       'df -h'
-abbr --add du       'du -h --max-depth=1'
-abbr --add j        'jobs'
-abbr --add g        'grep -Pi --color=auto'
-abbr --add ps       'ps uT --forest'
-abbr --add pgrep    'pgrep -fau (whoami)'
-abbr --add pkill    'pkill -fu (whoami)'
-abbr --add kill     'kill -s TERM'
-abbr --add fzfdo    'fzf | read -l fzf; and'
-abbr --add less     'less -iR'
-abbr --add netstat  'netstat --listening --program --numeric'
-abbr --add jq       'jq --color-output --sort-keys'
-abbr --add watch    'watch -cd -n 5'
+abbr --add df      'df -h'
+abbr --add du      'du -h --max-depth=1'
+abbr --add j       'jobs'
+abbr --add g       'grep -Pi --color=auto'
+abbr --add ps      'ps uT --forest'
+abbr --add pgrep   'pgrep -fau (whoami)'
+abbr --add pkill   'pkill -fu (whoami)'
+abbr --add kill    'kill -s TERM'
+abbr --add fzfdo   'fzf | read -l fzf; and'
+abbr --add less    'less -iR'
+abbr --add netstat 'netstat --listening --program --numeric'
+abbr --add jq      'jq --color-output --sort-keys'
+abbr --add watch   'watch -cd -n 5'
+abbr --add time    'time -f \'Total Time: %E\nExit Code: %x\''
 
-set -l pytest_args  '-vv --capture no --exitfirst --failed-first tests/'
-abbr --add pytest   "pytest $pytest_args"
-abbr --add pytestw  "pytest-watch -- $pytest_args"
+set -l pytest_args '-vv --capture no --exitfirst --failed-first tests/'
+abbr --add pytest  "pytest $pytest_args"
+abbr --add pytestw "pytest-watch -- $pytest_args"
 
 set -l local_functions (dirname (status --current-filename))/abbr_local.fish
 if test -e $local_functions
