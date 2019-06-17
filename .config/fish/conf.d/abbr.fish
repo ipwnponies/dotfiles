10e12d5fb2ebd81ec438ef52f3012d239ade4c79
John Nguyen <ipwnponies@gmail.com>
2019-06-16 23:30:57 -0700

    [fish] Convert abbr from universal to global
    Not sure why they are universal by default. Global sets them only for
    the current shell. Which makes it easy to modify and deprecate.

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -1,21 +1,21 @@
 # Abbreviations
-abbr --add df      'df -h'
-abbr --add du      'du -h --max-depth=1'
-abbr --add j       'jobs'
-abbr --add g       'grep -Pi --color=auto'
-abbr --add pgrep   'pgrep -fau (whoami)'
-abbr --add pkill   'pkill -fu (whoami)'
-abbr --add kill    'kill -s TERM'
-abbr --add fzfdo   'fzf | read -l fzf; and'
-abbr --add less    'less -iR'
-abbr --add netstat 'netstat --listening --program --numeric'
-abbr --add jq      'jq --color-output --sort-keys'
-abbr --add watch   'watch -cd -n 5'
-abbr --add time    'time -f \'Total Time: %E\nExit Code: %x\''
+abbr --global --add df      'df -h'
+abbr --global --add du      'du -h --max-depth=1'
+abbr --global --add j       'jobs'
+abbr --global --add g       'grep -Pi --color=auto'
+abbr --global --add pgrep   'pgrep -fau (whoami)'
+abbr --global --add pkill   'pkill -fu (whoami)'
+abbr --global --add kill    'kill -s TERM'
+abbr --global --add fzfdo   'fzf | read -l fzf; and'
+abbr --global --add less    'less -iR'
+abbr --global --add netstat 'netstat --listening --program --numeric'
+abbr --global --add jq      'jq --color-output --sort-keys'
+abbr --global --add watch   'watch -cd -n 5'
+abbr --global --add time    'time -f \'Total Time: %E\nExit Code: %x\''
 
 set -l pytest_args '--capture no --exitfirst --failed-first --testmon --tlf tests/'
-abbr --add pytest  "pytest -vv $pytest_args"
-abbr --add pytestw "pytest-watch -- $pytest_args"
+abbr --global --add pytest  "pytest -vv $pytest_args"
+abbr --global --add pytestw "pytest-watch -- $pytest_args"
 
 set -l local_functions (dirname (status --current-filename))/abbr_local.fish
 if test -e $local_functions
