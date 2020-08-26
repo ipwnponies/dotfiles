bbde24ff08b924f242b666fd9f6ada6c4d6e9307
John Nguyen <ipwnponies@gmail.com>
2020-08-26 11:19:06 -0700

    [fish] Fix taskwarrior completion for local install
    Source the upstream `task` completions from either
    `/usr/local/share/fish/completions` or `/usr/share/fish/completions`

--- .config/fish/completions/task.fish
+++ .config/fish/completions/task.fish
@@ -1,4 +1,7 @@
-source /usr/share/fish/completions/task.fish
+set -l completion_dir /usr/share/fish/completions/
+test -d $completion_dir; or set completion_dir /usr/local/share/fish/vendor_completions.d/
+
+source $completion_dir/task.fish
 function __fish.task.list.project
     task _unique project
 end
