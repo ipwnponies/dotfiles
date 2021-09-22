4736c2be4e1f34c6af339c41c3732683c563e91f
John Nguyen <ipwnponies@gmail.com>
2021-09-21 19:14:53 -0700

    [fish] Revamp ssh-agent and keys
    There are solutions that implement our gross hacky scripts, so use them
    instead.
    - macos should use keychain integration
    - linux and wsl should use keychain package

--- .config/fish/conf.d/00-ssh_agent.fish
+++ .config/fish/conf.d/00-ssh_agent.fish
@@ -1,36 +1,14 @@
-# Start up ssh-agent and load ssh keys
-if status --is-login; and status --is-interactive
-    set ssh_env_file $HOME/.ssh/ssh-agent-env
-
-    function start_agent -d 'Start ssh-agent and store env vars for other shells to reuse'
-        ssh-agent -c | sed 's/^echo/#/' > $ssh_env_file
-        chmod 600 $ssh_env_file
-        source $ssh_env_file
+# Load ssh keys
+if status --is-interactive
+    if test (uname -o) -eq 'Darwin'
+        # macos ssh can use OS keychain, no need for ssh-agent
+        exit
     end
 
-    function startup
-        # Key is already loaded, nothing more to do
-        if ssh-add -l >/dev/null 2>&1
-            return
-        end
-
-        # Try loading existing existing agent
-        if test -f $ssh_env_file
-            source $ssh_env_file
-        end
-
-        # Test if existing agent has not gone stale
-        ssh-add -l >/dev/null 2>&1; set sshadd_status $status
-
-        if test $sshadd_status -eq 2;
-            # No ssh-agent running
-            start_agent
-            ssh-add
-        else if test $sshadd_status -eq 1;
-            # No keys loaded in ssh-agent
-            ssh-add
-        end
+    if not type -q keychain
+        echo 'Install keychain to manage ssh-agent'
+        exit
     end
 
-    startup
+    keychain --eval --quiet --quick | source
 end
