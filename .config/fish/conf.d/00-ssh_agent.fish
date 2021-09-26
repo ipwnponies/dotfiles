33336dd44ce48f3fca12a1c5b6a783e84d0a9f49
John Nguyen <ipwnponies@gmail.com>
2021-09-26 16:38:16 -0700

    [fish] Fix ssh-agent script
    Fix to use string comparison, not numeric.. Derp

--- .config/fish/conf.d/00-ssh_agent.fish
+++ .config/fish/conf.d/00-ssh_agent.fish
@@ -1,6 +1,6 @@
 # Load ssh keys
 if status --is-interactive
-    if test (uname -o) -eq 'Darwin'
+    if test (uname -o) = 'Darwin'
         # macos ssh can use OS keychain, no need for ssh-agent
         exit
     end
