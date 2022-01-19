50123f76738b522faada0784bd1d04e24a47f8b3
John Nguyen <ipwnponies@gmail.com>
2022-01-19 01:42:09 -0800

    [fish] Add poetry completions

---
+++ .config/fish/conf.d/poetry.fish
@@ -0,0 +1,13 @@
+if status --is-interactive
+    if not type -q poetry
+        echo 'poetry is not installed'
+        exit
+    end
+
+    set completion_path (dirname (status filename))/../completions/poetry.fish
+
+    if not test -e $completion_path
+        echo 'Installing poetry completions'
+        poetry completions > $completion_path
+    end
+end
