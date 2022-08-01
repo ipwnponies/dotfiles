0b74ed573183c9df555325513d7585b0b44cabb6
John Nguyen <ipwnponies@gmail.com>
2022-08-01 10:03:02 -0700

    [venv] Remove vendored venv-update

--- .config/fish/conf.d/20-virtualenv.fish
+++ .config/fish/conf.d/20-virtualenv.fish
@@ -5,10 +5,7 @@ set PATH "$venv/bin" $PATH
 
 if status --is-login; and status --is-interactive; and type -q virtualenv;
 
-    set venv_update "$XDG_CONFIG_HOME/venv-update/venv-update"
     set requirements "$XDG_CONFIG_HOME/venv-update/requirements.txt"
-    set requirements_dev "$XDG_CONFIG_HOME/venv-update/requirements-dev.txt"
-    set requirements_bootstrap "$XDG_CONFIG_HOME/venv-update/requirements-bootstrap.txt"
     set logfile "$XDG_CACHE_HOME/venv-update/log"
 
 
@@ -16,15 +13,9 @@ if status --is-login; and status --is-interactive; and type -q virtualenv;
     # When it does change something, we only need the side effects (new programs installed)
     fish -c "
         mkdir -p (dirname $logfile)
-        if not $venv_update \
-                venv= -p python3 $venv \
-                bootstrap-deps= -r $requirements_bootstrap \
-                install= -r $requirements -r $requirements_dev \
-                pip-command= pip-faster install --upgrade --prune --prefer-binary \
-             >> $logfile
-            echo 'Uh... that didn\'t work gud. So check out $logfile'
-        end
-    " &
+        /usr/bin/virtualenv --python python3.9 $venv
+        $venv/bin/pip install -r $requirements
+    " >> $logfile &
 end
 
 # Add pyenv shims if this system supports it
