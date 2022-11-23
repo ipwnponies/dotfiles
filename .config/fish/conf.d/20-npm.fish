50cba310e006c3c28c484e2325c25c0cc0b92b51
John Nguyen <ipwnponies@gmail.com>
2022-11-23 10:48:42 -0800

    [npm] Use npm lockfile v3
    This requires npm v7 (released 2021-02), which introduced v3. npm has a
    backwards compatible mode that doubles the lockfile size.

--- .config/fish/conf.d/20-npm.fish
+++ .config/fish/conf.d/20-npm.fish
@@ -5,11 +5,17 @@ fish_add_path --global $venv/node_modules/.bin
 
 if status --is-login; and status --is-interactive; and type -q npm;
 
+    set npm_version (string split '.' (npm --version))
+    if not test (count $npm_version) -eq 3; or not test $npm_version[1] -ge 7
+        echo 'npm 7+ is required' >&2
+        exit
+    end
+
     set logfile "$XDG_CACHE_HOME/npm"
 
     fish -c "
         mkdir -p (dirname $logfile)
-        if not npm install --loglevel=error --prefix $venv >> $logfile 2>&1
+        if not npm install --lockfile-version 3 --loglevel=error --prefix $venv >> $logfile 2>&1
             echo 'Uh... that didn\'t work gud. So check out $logfile'
         end
     " &
