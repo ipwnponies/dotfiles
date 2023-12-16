3cf1539b5bfb6a9a09781ab98615a91dac77dc6e
John Nguyen <ipwnponies@gmail.com>
2023-12-15 18:30:56 -0800

    [fish] Add navi keybinding
    Add <c-g> keybinding to invoke navi.
    It will output result on the commandline, will not run.

--- .config/fish/conf.d/navi.fish
+++ .config/fish/conf.d/navi.fish
@@ -1,1 +1,12 @@
 set --export NAVI_CONFIG $XDG_CONFIG_HOME/navi/config.yaml
+
+
+# This is trimmed down custom `navi widget fish`
+function _navi_smart_replace
+    set -l current_process (commandline -p | string trim)
+
+    commandline (navi --print --query "$current_process")
+    commandline -f repaint
+end
+
+bind \cg _navi_smart_replace
