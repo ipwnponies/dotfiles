793034ee43ed7ae0453ea8a9a876261340b4b1b4
John Nguyen <ipwnponies@gmail.com>
2024-01-02 13:27:09 -0800

    [fish] Fix task custom completions to support apple M1
    Brew is installed in a new location for apple M1/M2 laptops.
    Fix by adding yet another fallback.

--- .config/fish/completions/task.fish
+++ .config/fish/completions/task.fish
@@ -1,7 +1,16 @@
 set -l completion_dir /usr/share/fish/completions/
 test -d $completion_dir; or set completion_dir /usr/local/share/fish/vendor_completions.d/
+# Brew installation has changed for apple silicon, now in /opt/homebrew
+test -d $completion_dir; or set completion_dir /opt/homebrew/share/fish/vendor_completions.d/
+
+if test ! -d $completion_dir
+    echo "Could not find fish vendor completion dir"
+    exit
+end
 
 source $completion_dir/task.fish
+
+# Shadow upstream function. Add filter for recent or active projects
 function __fish.task.list.project
     task +PENDING or end.after:'today - 3 month' _unique project
 end
