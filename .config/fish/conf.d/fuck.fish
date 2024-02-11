dfeaba02dff035cc3c5f6f431ab2fa0f49b7904b
John Nguyen <ipwnponies@gmail.com>
2024-02-11 11:23:13 -0800

    [fish] Add thefuck
    We have not used thefuck in years... But now is the time to properly check in the settings and keybindings.

---
+++ .config/fish/conf.d/fuck.fish
@@ -0,0 +1,3 @@
+# The default binding \cf is forward character. I hope to god I don't have to resort to this.
+# It's like the 90s all over again, vt420 and all.
+bind \cf 'fuck; commandline -f repaint'
--- .config/fish/functions/fuck.fish
+++ .config/fish/functions/fuck.fish
@@ -1,9 +1,1 @@
-function fuck -d "Correct your previous console command"
-  set -l fucked_up_command $history[1]
-  env TF_ALIAS=fuck PYTHONIOENCODING=utf-8 thefuck $fucked_up_command | read -l unfucked_command
-  if [ "$unfucked_command" != "" ]
-    eval $unfucked_command
-    builtin history delete --exact --case-sensitive -- $fucked_up_command
-    builtin history merge 2>&1 /dev/null
-  end
-end
+thefuck --alias | source
