43e2443193c537554cc7511143a294860f892abe
John Nguyen <ipwnponies@gmail.com>
2024-02-28 16:05:28 -0800

    [fish] Enhance copy_command_output to run last command
    If used with 0 args, then rerun the last command from history.

--- .config/fish/functions/copy_command_output_clipboard.fish
+++ .config/fish/functions/copy_command_output_clipboard.fish
@@ -1,4 +1,10 @@
 function copy_command_output_clipboard
+    if test (count $argv) -eq 0
+        set cmd (string split ' ' (history --max 1))
+    else
+        set cmd $argv
+    end
+
     set stdout (mktemp)
     echo -e "\$ $argv\n" >> $stdout
     $argv >> $stdout
