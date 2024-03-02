a774d40f51fd1e08b2caf7c6ba237354314fe416
John Nguyen <ipwnponies@gmail.com>
2024-03-01 18:48:11 -0800

    [fish] Set XDG_STATE_HOME
    This is newly added to XDG. It's for user state persist between runs.
    This differs from cache, which is intended for data files that are
    cached.

--- .config/fish/conf.d/10-env.fish
+++ .config/fish/conf.d/10-env.fish
@@ -4,6 +4,7 @@
 set -x XDG_DATA_HOME $HOME/.local/share
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_CACHE_HOME $HOME/.cache
+set -x XDG_STATE_HOME $HOME/.local/state
 
 fish_add_path --prepend --global $HOME/bin
 
