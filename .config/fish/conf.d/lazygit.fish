3ab32019e8eb1a8ecf0e786e121c536b9b045213
John Nguyen <ipwnponies@gmail.com>
2024-06-14 09:44:41 -0700

    [fish] Conditionally set custom lazygit config
    lazygit fails if a file doesn't exist. So we should only conditionally
    set if we detect a custom config exists.

--- .config/fish/conf.d/lazygit.fish
+++ .config/fish/conf.d/lazygit.fish
@@ -1,4 +1,9 @@
 if status --is-interactive; and type -q lazygit
     set --local lazygit_dir "$XDG_CONFIG_HOME/lazygit"
-    set --export LG_CONFIG_FILE "$lazygit_dir/config.yml,$lazygit_dir/config_local.yaml"
+    set --local default $lazygit_dir/config.yml
+    set --local custom $lazygit_dir/config_local.yaml
+
+    if test -O $custom
+        set --export LG_CONFIG_FILE $default,$custom
+    end
 end
