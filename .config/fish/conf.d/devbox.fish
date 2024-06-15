db6e6687720bdad3c151184298a192fc4620c113
John Nguyen <ipwnponies@gmail.com>
2024-06-15 00:42:39 -0700

    [fish] Add devbox fish setup to conf.d

--- .config/fish/conf.d/devbox.fish
+++ .config/fish/conf.d/devbox.fish
@@ -1,2 +1,8 @@
+# Vendor completions
 devbox completion fish | source
+
+# Sync dependencies
 status --is-login; and devbox --config $XDG_CONFIG_HOME/devbox/devbox.json install
+
+# Add devbox to PATH
+devbox --config $XDG_CONFIG_HOME/devbox/devbox.json shellenv | source
