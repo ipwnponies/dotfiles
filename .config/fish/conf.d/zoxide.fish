ef96dccc4706e088a057bc4478f5878c81f308a0
John Nguyen <ipwnponies@gmail.com>
2024-08-27 15:48:33 -0700

    [fish] Shadow zoxide cd with custom wrapper
    fzf-widget has bindings that call `cd -- <PATH>`.
    zoxide does not correctly support this part of teh cd interface

--- .config/fish/conf.d/zoxide.fish
+++ .config/fish/conf.d/zoxide.fish
@@ -2,4 +2,12 @@ if status --is-interactive
     set --export _ZO_FZF_OPTS "$FZF_DEFAULT_OPTS --keep-right --border=rounded --no-sort"
 
     zoxide init fish --cmd cd | source
+
+    # fzf-widget calls `cd -- $argv` and zoxide doesn't understand '--'
+    function cd --wraps=__zoxide_z --description 'Override default zoxide cd alias'
+        if test $argv[1] = '--'
+            set --erase argv[1]
+        end
+        __zoxide_z $argv
+    end
 end
