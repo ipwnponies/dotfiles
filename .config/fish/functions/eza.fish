19954b40b3dd726a7e195d2a0a0e0cce61a5b1bd
John Nguyen <ipwnponies@gmail.com>
2024-10-01 15:56:55 -0700

    [fish] Refresh eza options
    Add --hyperlink option. This is to open in file explorer.
    Split options to match categorization from eza README: explicitly add display category.

--- .config/fish/functions/eza.fish
+++ .config/fish/functions/eza.fish
@@ -1,5 +1,6 @@
 function eza -d 'eza but with default options'
-    # This is notably missing the --long option. These options are inert otherwise, which is the default for exa but
-    set long_options --grid --colour-scale --git --time-style=long-iso
-    command eza --classify --icons $long_options $argv
+    # This is notably missing the --long option. These options are inert otherwise, which is the default for eza
+    set display_options --grid --classify --colour-scale --icons --hyperlink
+    set long_options --time-style=long-iso --header
+    command eza $display_options $long_options $argv
 end
