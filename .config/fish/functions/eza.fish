3a845f0eb4e56553faa3fc08022dc417e829626d
John Nguyen <ipwnponies@gmail.com>
2025-03-26 20:59:16 -0700

    [fish] Fix eza wrapper for handling pipes
    Make eza wrapper tty-aware, as the wrapper is standing in the middle of
    interactive tty and eza.

--- .config/fish/functions/eza.fish
+++ .config/fish/functions/eza.fish
@@ -1,4 +1,9 @@
 function eza -d 'eza but with default options'
+    if not isatty stdout
+        command eza $argv
+        return
+    end
+
     # This is notably missing the --long option. These options are inert otherwise, which is the default for eza
     set display_options --grid --classify --colour-scale --icons --hyperlink
     set long_options --time-style=long-iso --header
