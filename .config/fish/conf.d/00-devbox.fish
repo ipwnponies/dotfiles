df99a7570f16ad2cdc5910fc9642e36956779cd4
John Nguyen <ipwnponies@gmail.com>
2025-03-26 23:22:21 -0700

    [fish] Add devbox vendor completions to search path
    fish_complete_path is not mutable. But this works well enough to ensure
    interactive user gets the completion paths updated.

--- .config/fish/conf.d/00-devbox.fish
+++ .config/fish/conf.d/00-devbox.fish
@@ -6,3 +6,5 @@ devbox global shellenv --init-hook | source
 
 # Sync dependencies
 status --is-login; and devbox global install
+
+set --append fish_complete_path $DEVBOX_PACKAGES_DIR/share/fish/vendor_completions.d
