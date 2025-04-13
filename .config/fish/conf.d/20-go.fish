bb4f0277fd77bbbe26e7c4f9d8ea52cfd62cd923
John Nguyen <ipwnponies@gmail.com>
2025-04-12 21:46:32 -0700

    [devbox] Move jiq installation from go to devbox
    devbox.json is a declarative config, which is preferable to go install
    upon shell startup.

--- .config/fish/conf.d/20-go.fish
+++ .config/fish/conf.d/20-go.fish
@@ -2,7 +2,6 @@ set -gx GOPATH $XDG_DATA_HOME/go
 fish_add_path --global $GOPATH/bin
 
 if status --is-login; and status --is-interactive; and type -q go
-    type -q jiq; or go get github.com/fiatjaf/jiq/cmd/jiq
     type -q fac; or go get github.com/mkchoi212/fac
     type -q gron; or go get github.com/tomnomnom/gron
 end
