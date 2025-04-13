cc3e9995b1a879e29ae6c9e714723a4c84f94920
John Nguyen <ipwnponies@gmail.com>
2025-04-12 21:58:51 -0700

    [devbox] Move fac from go to devbox
    This is the last go install. So we can also remove the go install binary
    altogether.

--- .config/fish/conf.d/20-go.fish
+++
@@ -1,6 +0,0 @@
-set -gx GOPATH $XDG_DATA_HOME/go
-fish_add_path --global $GOPATH/bin
-
-if status --is-login; and status --is-interactive; and type -q go
-    type -q fac; or go get github.com/mkchoi212/fac
-end
