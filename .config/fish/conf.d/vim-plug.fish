0a568391f41c4e0d9016227dd20833c7f9589ff0
John Nguyen <ipwnponies@gmail.com>
2025-04-16 00:06:05 -0700

    [vim] Remove more coc cruft
    Why is this everywhere. It's like sand in your shoes.

--- .config/fish/conf.d/vim-plug.fish
+++ .config/fish/conf.d/vim-plug.fish
@@ -1,9 +1,6 @@
 # Installation of vim-plug plugins
 
 if status --is-login; and status --is-interactive;
-
-    set coc_extensions coc-css coc-flow coc-html coc-json coc-python coc-tsserver coc-yaml
-
     # Async installation of plugins to speed up shell startup
     # stdin and stdout are redirected to the void, to signify this is non-interactive vim session
     fish -c "$EDITOR -u $HOME/.vimrc -c PlugInstall -c qa </dev/null >/dev/null 2>&1" &
