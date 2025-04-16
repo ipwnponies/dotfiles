6ab114e5046bc646776988dfe75600b5682068aa
John Nguyen <ipwnponies@gmail.com>
2025-04-15 23:21:56 -0700

    [fish] Replace thefuck with pay-respects setup
    Unfortunately the module pay-respects runtime is not available on
    nixpkgs. So we need to set up cargo.

---
+++ .config/fish/conf.d/20-cargo.fish
@@ -0,0 +1,9 @@
+function main
+    set -x CARGO_INSTALL_ROOT $XDG_DATA_HOME/cargo
+    set -x CARGO_TARGET_DIR $XDG_CACHE_HOME/cargo
+
+    fish_add_path $CARGO_INSTALL_ROOT/bin
+    cargo install pay-respects-module-runtime-rules
+end
+
+status --is-login; and main
--- .config/fish/conf.d/fuck.fish
+++ .config/fish/conf.d/fuck.fish
@@ -1,3 +1,4 @@
 # The default binding \cf is forward character. I hope to god I don't have to resort to this.
 # It's like the 90s all over again, vt420 and all.
-bind \cf 'fuck; commandline -f repaint'
+pay-respects fish --alias f | source
+bind \cf 'f; commandline -f repaint'
