2159405e991932fa4ec6d6fc93efab5452585dec
John Nguyen <ipwnponies@gmail.com>
2025-04-20 10:35:59 -0700

    [fish] Set default FZF option to use double border

--- .config/fish/conf.d/fzf.fish
+++ .config/fish/conf.d/fzf.fish
@@ -1,6 +1,6 @@
 if status --is-interactive; and type -q fzf
     set -gx FZF_DEFAULT_OPTS "--reverse --select-1 --exit-0 \
-    --border --height 40% --min-height 10 --margin 1,5 --info inline \
+    --border double --height 40% --min-height 10 --margin 1,5 --info inline \
     --bind pgdn:preview-page-down,pgup:preview-page-up,change:top,ctrl-s:toggle-sort,ctrl-a:toggle-all"
 
     fzf --fish | source
