4e3c4d26ca0d2f3cc16c4198a19825dfce392ccb
John Nguyen <ipwnponies@gmail.com>
2025-04-20 10:33:24 -0700

    [fish] Set up fzf shell integration
    FZF keybindings used to be sourcing the function for fzf_key_binding.
    But there's a vendored way to do this and no rely on symlinks and
    whatnot.

--- .config/fish/conf.d/fzf.fish
+++ .config/fish/conf.d/fzf.fish
@@ -2,4 +2,6 @@ if status --is-interactive; and type -q fzf
     set -gx FZF_DEFAULT_OPTS "--reverse --select-1 --exit-0 \
     --border --height 40% --min-height 10 --margin 1,5 --info inline \
     --bind pgdn:preview-page-down,pgup:preview-page-up,change:top,ctrl-s:toggle-sort,ctrl-a:toggle-all"
+
+    fzf --fish | source
 end
--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -2,6 +2,4 @@ function fish_user_key_bindings
     # Disable plugin-expand binding override, remap binding to something else
     bind --erase --sets-mode expand \t
     bind --sets-mode expand \cx expand:execute
-
-    fzf_key_bindings
 end
