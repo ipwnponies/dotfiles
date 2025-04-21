594500c98f367aa3ad8ce593e6653ada61be4284
John Nguyen <ipwnponies@gmail.com>
2025-04-20 20:22:38 -0700

    [pay-respects] Remove thefuck rules and config
    We switched to pay-respects.

--- .config/thefuck/rules/git_br_delete.py
+++
@@ -1,9 +0,0 @@
-from thefuck.utils import replace_argument
-
-
-def match(command):
-    return ('br -d' in command.script and 'If you are sure you want to delete it' in command.stderr)
-
-
-def get_new_command(command):
-    return replace_argument(command.script, '-d', '-D')
