49198c7583cebc8588a581e1c3c96a2745bc67f9
John Nguyen <ipwnponies@gmail.com>
2025-04-20 23:36:41 -0700

    [fish] Always set cargo env vars
    Set env vars and paths so that the binaries can be used
    non-interactively.

--- .config/fish/conf.d/20-cargo.fish
+++ .config/fish/conf.d/20-cargo.fish
@@ -1,8 +1,8 @@
-function main
-    set -x CARGO_INSTALL_ROOT $XDG_DATA_HOME/cargo
-    set -x CARGO_TARGET_DIR $XDG_CACHE_HOME/cargo
+set -x CARGO_INSTALL_ROOT $XDG_DATA_HOME/cargo
+set -x CARGO_TARGET_DIR $XDG_CACHE_HOME/cargo
+fish_add_path $CARGO_INSTALL_ROOT/bin --append
 
-    fish_add_path $CARGO_INSTALL_ROOT/bin
+function main
     not type --query _pay-respects-module-100-runtime-rules; and cargo install pay-respects-module-runtime-rules
 end
 
