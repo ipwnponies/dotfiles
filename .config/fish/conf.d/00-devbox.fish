12bd662f876093704ace3bbdc4bf9610f54fd0e4
John Nguyen <ipwnponies@gmail.com>
2025-04-22 00:17:04 -0700

    [fish] Tune shell startup devbox costs
    Use statically generated completions. These are regenerated from devbox
    command periodically.
    Only call `devbox shellenv` during interactive shells. Any further
    subshell will inherit the PATH. We don't have a use case yet of
    non-interactive shell with no inheritance, this is not handled and known
    deficiency.

--- .config/fish/conf.d/00-devbox.fish
+++ .config/fish/conf.d/00-devbox.fish
@@ -1,18 +1,33 @@
 function main
-    # Add devbox to PATH
-    devbox global shellenv --init-hook | source
-
     if status --is-login
         # Sync dependencies
-        status --is-login; and devbox global install
+        devbox global install
     end
 
     if status --is-interactive
+
+        # Add devbox to PATH
+        devbox global shellenv | source
+
         # Vendor completions
-        devbox completion fish | source
+        set completion $XDG_CONFIG_HOME/fish/completions/devbox.fish
+
+        # Statically compile completion, with TTL
+        set ttl (math '60*60*24*7') # 1 week
+        function calc_age
+            set file_age (stat -c %Y $argv[1])
+            set current_time (date +%s)
+            math $current_time - $file_age
+        end
+
+        if test ! -f $completion -o (calc_age $completion) -gt $ttl
+            echo 'Regenerating devbox completion'
+            devbox completion fish > $completion
+        end
+
         set --append fish_complete_path $DEVBOX_PACKAGES_DIR/share/fish/vendor_completions.d
     end
 
 end
 
-main
+time main
