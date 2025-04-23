d65aa64decea9835289a1df8b23f3f678b18bd93
John Nguyen <ipwnponies@gmail.com>
2025-04-22 22:59:08 -0700

    [fish] Add TTL caching to devbox
    Instead of running devbox to generate completions and shell envs, we
    cache the output with TTL. This speeds up shell startup.
    Shell env is mostly PATH and other env vars that are host specific. But
    they don't change frequently, so functionally unaffected for purpose of
    installing packages.

--- .config/fish/conf.d/10-devbox.fish
+++ .config/fish/conf.d/10-devbox.fish
@@ -1,33 +1,40 @@
 function main
-    if status --is-login
-        # Sync dependencies
-        devbox global install
-    end
 
-    if status --is-interactive
-
-        # Add devbox to PATH
-        devbox global shellenv | source
-
-        # Vendor completions
-        set completion $XDG_CONFIG_HOME/fish/completions/devbox.fish
-
-        # Statically compile completion, with TTL
+    function is_expired
+        # Expire dynamic generations after a week
         set ttl (math '60*60*24*7') # 1 week
-        function calc_age
-            set file_age (stat -c %Y $argv[1])
-            set current_time (date +%s)
-            math $current_time - $file_age
-        end
+        set file $argv[1]
 
-        if test ! -f $completion -o (calc_age $completion) -gt $ttl
-            echo 'Regenerating devbox completion'
-            devbox completion fish > $completion
-        end
+        set file_age (stat -c %Y $file)
+        set current_time (date +%s)
 
-        set --append fish_complete_path $DEVBOX_PACKAGES_DIR/share/fish/vendor_completions.d
+        test ! -f $file; or test (math $current_time - $file_age) -gt $ttl
     end
 
+    # Add devbox to PATH
+    set generated_config $XDG_CONFIG_HOME/fish/conf.d/10-devbox-generated_local.fish
+
+    if is_expired $generated_config
+        echo 'Regenerating devbox config'
+        devbox global shellenv > $generated_config
+    end
+
+    # Vendor completions
+    set -l generated_completion $XDG_CONFIG_HOME/fish/completions/devbox.fish
+
+
+    if is_expired $generated_completion
+        echo 'Regenerating devbox completion'
+        devbox completion fish > $generated_completion
+    end
+
+    set --append fish_complete_path $DEVBOX_PACKAGES_DIR/share/fish/vendor_completions.d
 end
 
-time main
+function install
+    # Sync dependencies
+    devbox global install
+end
+
+status --is-login; and install
+status --is-interactive; and main
