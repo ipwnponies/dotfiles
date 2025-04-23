d47e885c94d2cbaeb481db6d096f5624fad8547b
John Nguyen <ipwnponies@gmail.com>
2025-04-22 22:38:51 -0700

    [fish] Simplify npm setup
    devbox installs node, so no need to check if npm exists.

--- .config/fish/conf.d/20-npm.fish
+++ .config/fish/conf.d/20-npm.fish
@@ -1,18 +1,10 @@
 # Installation of local npm library
 
-set -l venv "$XDG_CONFIG_HOME/npm"
-fish_add_path --global $venv/node_modules/.bin
-
-if status --is-login; and status --is-interactive; and type -q npm;
-
-    set npm_version (string split '.' (npm --version))
-    if not test (count $npm_version) -eq 3; or not test $npm_version[1] -ge 7
-        echo 'npm 7+ is required' >&2
-        exit
-    end
+function install
+    set venv "$XDG_CONFIG_HOME/npm"
+    fish_add_path --global $venv/node_modules/.bin
 
     set logfile "$XDG_CACHE_HOME/npm"
-
     fish -c "
         mkdir -p (dirname $logfile)
         if not npm install --lockfile-version 3 --loglevel=error --prefix $venv >> $logfile 2>&1
@@ -20,3 +12,5 @@ if status --is-login; and status --is-interactive; and type -q npm;
         end
     " &
 end
+
+status --is-login; and install
