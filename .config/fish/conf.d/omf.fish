7bc996cdaa67f414c2f970c89dbe4c0c68ebb5dc
John Nguyen <ipwnponies@gmail.com>
2025-04-22 22:43:34 -0700

    [fish] Refactor omf config
    Structure into install time and runtime conf.d. Install time is
    infrequent, as it's slow (800 ms).
    In fact, we also add TTL cache logic to minimize frequency of running
    `omf install`.

--- .config/fish/conf.d/omf.fish
+++ .config/fish/conf.d/omf.fish
@@ -1,30 +1,45 @@
-if status --is-interactive;
-    set -l omf_conf $XDG_DATA_HOME/omf/
-    set -l logfile $XDG_CACHE_HOME/omf/log
+function main
+    set omf_conf $XDG_DATA_HOME/omf/
 
     if test -d $omf_conf
-        # Configure oh-my-fish
-
-        set -gx OMF_PATH "$XDG_DATA_HOME/omf";
-
         # Load Oh My Fish configuration.
-        source $OMF_PATH/init.fish
+        source $omf_conf/init.fish
+    end
 
-        # Install all omf plugins upon login
-        if status --is-login
-            # This can introduce issues wth newly installed packages They won't work correctly until the next shell
-            # startup This is acceptable because it's infrequent and not worth paying on every shell startup.
-            omf install >> $logfile &
-        end
+end
 
-    else if status --is-login
+function install
+    set omf_conf $XDG_DATA_HOME/omf/
+
+    function is_expired
+        # Expire dynamic generations after a week
+        set ttl (math '60*60*24*7') # 1 week
+        set file $argv[1]
+
+        set file_age (stat -c %Y $file)
+        set current_time (date +%s)
+
+        test ! -f $file; or test (math $current_time - $file_age) -gt $ttl
+    end
+
+    if test -d $omf_conf
+        set bundle $XDG_CONFIG_HOME/omf/bundle
+
+        # This can introduce issues wth newly installed packages They won't work correctly until the next shell
+        # startup This is acceptable because it's infrequent and not worth paying on every shell startup.
+
+        set logfile $XDG_CACHE_HOME/omf/log
+        is_expired $bundle; and omf install
+    else
         # This is used to undo clobbering by the omf installer
-        set -l current_file (status --current-filename)
+        set current_file (status --current-filename)
 
-        eval $XDG_CONFIG_HOME/fish/plugins/oh-my-fish/bin/install --noninteractive --offline
+        $XDG_CONFIG_HOME/fish/plugins/oh-my-fish/bin/install --noninteractive --offline
         git checkout $current_file
 
         omf reload
     end
-
 end
+
+status --is-interactive; and main
+status --is-login; and status --is-interactive; and install
