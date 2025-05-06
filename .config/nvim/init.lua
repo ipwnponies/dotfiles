82886809b6a1dd5d4c08812074966a534018c304
John Nguyen <ipwnponies@gmail.com>
2025-05-05 22:39:35 -0700

    [vim] Stub out init.lua for incremental migration
    Stub out placeholders for migrating vim configs from vimscript to lua.

---
+++ .config/nvim/init.lua
@@ -0,0 +1,8 @@
+-- init.lua
+require('settings')  -- basic options
+require('keymaps')   -- key mappings
+require('plugins')   -- plugin manager and plugins
+
+local xdg_config_home = os.getenv("XDG_CONFIG_HOME") or "~/.config"
+-- Fallthrough to loading original vimscript
+vim.cmd('source ' .. xdg_config_home .. '/nvim/init.vim')
