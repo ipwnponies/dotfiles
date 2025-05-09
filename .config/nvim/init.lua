8c683f7a71c19db1fc942c24f46ea761d84b87b5
John Nguyen <ipwnponies@gmail.com>
2025-05-08 23:52:48 -0700

    [vim] Install lazy.nvim package manager

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,7 @@
 -- init.lua
 require('settings')  -- basic options
 require('keymaps')   -- key mappings
-require('plugins')   -- plugin manager and plugins
+require('config.lazy')   -- plugin manager and plugins
 
 local xdg_config_home = os.getenv("XDG_CONFIG_HOME") or "~/.config"
 -- Fallthrough to loading original vimscript
