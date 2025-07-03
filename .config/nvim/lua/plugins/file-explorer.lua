b98d2cf102b39e1cece71310ea68bba7d7a04706
John Nguyen <ipwnponies@gmail.com>
2025-07-03 10:57:17 -0700

    [nvim] Fix neotree config
    Oops, missing closing brace resulted in portion of config being nested
    inside sibling instead.

--- .config/nvim/lua/plugins/file-explorer.lua
+++ .config/nvim/lua/plugins/file-explorer.lua
@@ -39,7 +39,8 @@ return {
 		},
 		lazy = false,
 		config = function()
-			require("neo-tree").setup({
+			--- @type neotree.Config
+			local neotree_config = {
 				close_if_last_window = false,
 				popup_border_style = "NC",
 				enable_git_status = true,
@@ -189,102 +190,102 @@ return {
 							},
 						},
 					},
-					nesting_rules = {},
-					filesystem = {
-						filtered_items = {
-							visible = false, -- when true, they will just be displayed differently than normal items
-							hide_dotfiles = true,
-							hide_gitignored = true,
-							hide_hidden = true, -- only works on Windows for hidden files/directories
-							hide_by_name = {
-								"node_modules",
-							},
-							hide_by_pattern = { -- uses glob style patterns
-								--"*.meta",
-								--"*/src/*/tsconfig.json",
-							},
-							always_show = { -- remains visible even if other settings would normally hide it
-								--".gitignored",
-							},
-							always_show_by_pattern = { -- uses glob style patterns
-								--".env*",
-							},
-							never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
-								--".DS_Store",
-								--"thumbs.db"
-							},
-							never_show_by_pattern = { -- uses glob style patterns
-								--".null-ls_*",
-							},
+				},
+				nesting_rules = {},
+				filesystem = {
+					filtered_items = {
+						visible = false, -- when true, they will just be displayed differently than normal items
+						hide_dotfiles = true,
+						hide_gitignored = true,
+						hide_hidden = true, -- only works on Windows for hidden files/directories
+						hide_by_name = {
+							"node_modules",
 						},
-						follow_current_file = {
-							enabled = false,
-							leave_dirs_open = false,
+						hide_by_pattern = { -- uses glob style patterns
+							--"*.meta",
+							--"*/src/*/tsconfig.json",
 						},
-						group_empty_dirs = false,
-						hijack_netrw_behavior = "open_default",
-						use_libuv_file_watcher = true,
-						window = {
-							mappings = {
-								["<bs>"] = "navigate_up",
-								["."] = "set_root",
-								["H"] = "toggle_hidden",
-								["/"] = "fuzzy_finder",
-								["D"] = "fuzzy_finder_directory",
-								["#"] = "fuzzy_sorter", -- fuzzy sorting using the fzy algorithm
-								["f"] = "filter_on_submit",
-								["<c-x>"] = "clear_filter",
-								["[g"] = "prev_git_modified",
-								["]g"] = "next_git_modified",
-								["o"] = {
-									"show_help",
-									nowait = false,
-									config = { title = "Order by", prefix_key = "o" },
-								},
-								["oc"] = { "order_by_created", nowait = false },
-								["od"] = { "order_by_diagnostics", nowait = false },
-								["og"] = { "order_by_git_status", nowait = false },
-								["om"] = { "order_by_modified", nowait = false },
-								["on"] = { "order_by_name", nowait = false },
-								["os"] = { "order_by_size", nowait = false },
-								["ot"] = { "order_by_type", nowait = false },
-								-- ['<key>'] = function(state) ... end,
-							},
-							fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
-								["<down>"] = "move_cursor_down",
-								["<C-n>"] = "move_cursor_down",
-								["<up>"] = "move_cursor_up",
-								["<C-p>"] = "move_cursor_up",
-								["<esc>"] = "close",
-							},
+						always_show = { -- remains visible even if other settings would normally hide it
+							--".gitignored",
+						},
+						always_show_by_pattern = { -- uses glob style patterns
+							--".env*",
+						},
+						never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+							--".DS_Store",
+							--"thumbs.db"
+						},
+						never_show_by_pattern = { -- uses glob style patterns
+							--".null-ls_*",
 						},
 					},
-					buffers = {
-						follow_current_file = {
-							enabled = true, -- This will find and focus the file in the active buffer every time
-							--              -- the current file is changed while the tree is open.
-							leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
-						},
-						group_empty_dirs = true, -- when true, empty folders will be grouped together
-						show_unloaded = true,
-						window = {
-							mappings = {
-								["d"] = "buffer_delete",
-								["bd"] = "buffer_delete",
-								["<bs>"] = "navigate_up",
-								["."] = "set_root",
-								["o"] = {
-									"show_help",
-									nowait = false,
-									config = { title = "Order by", prefix_key = "o" },
-								},
-								["oc"] = { "order_by_created", nowait = false },
-								["od"] = { "order_by_diagnostics", nowait = false },
-								["om"] = { "order_by_modified", nowait = false },
-								["on"] = { "order_by_name", nowait = false },
-								["os"] = { "order_by_size", nowait = false },
-								["ot"] = { "order_by_type", nowait = false },
+					follow_current_file = {
+						enabled = false,
+						leave_dirs_open = false,
+					},
+					group_empty_dirs = false,
+					hijack_netrw_behavior = "open_default",
+					use_libuv_file_watcher = true,
+					window = {
+						mappings = {
+							["<bs>"] = "navigate_up",
+							["."] = "set_root",
+							["H"] = "toggle_hidden",
+							["/"] = "fuzzy_finder",
+							["D"] = "fuzzy_finder_directory",
+							["#"] = "fuzzy_sorter", -- fuzzy sorting using the fzy algorithm
+							["f"] = "filter_on_submit",
+							["<c-x>"] = "clear_filter",
+							["[g"] = "prev_git_modified",
+							["]g"] = "next_git_modified",
+							["o"] = {
+								"show_help",
+								nowait = false,
+								config = { title = "Order by", prefix_key = "o" },
 							},
+							["oc"] = { "order_by_created", nowait = false },
+							["od"] = { "order_by_diagnostics", nowait = false },
+							["og"] = { "order_by_git_status", nowait = false },
+							["om"] = { "order_by_modified", nowait = false },
+							["on"] = { "order_by_name", nowait = false },
+							["os"] = { "order_by_size", nowait = false },
+							["ot"] = { "order_by_type", nowait = false },
+							-- ['<key>'] = function(state) ... end,
+						},
+						fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
+							["<down>"] = "move_cursor_down",
+							["<C-n>"] = "move_cursor_down",
+							["<up>"] = "move_cursor_up",
+							["<C-p>"] = "move_cursor_up",
+							["<esc>"] = "close",
+						},
+					},
+				},
+				buffers = {
+					follow_current_file = {
+						enabled = true, -- This will find and focus the file in the active buffer every time
+						--              -- the current file is changed while the tree is open.
+						leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+					},
+					group_empty_dirs = true, -- when true, empty folders will be grouped together
+					show_unloaded = true,
+					window = {
+						mappings = {
+							["d"] = "buffer_delete",
+							["bd"] = "buffer_delete",
+							["<bs>"] = "navigate_up",
+							["."] = "set_root",
+							["o"] = {
+								"show_help",
+								nowait = false,
+								config = { title = "Order by", prefix_key = "o" },
+							},
+							["oc"] = { "order_by_created", nowait = false },
+							["od"] = { "order_by_diagnostics", nowait = false },
+							["om"] = { "order_by_modified", nowait = false },
+							["on"] = { "order_by_name", nowait = false },
+							["os"] = { "order_by_size", nowait = false },
+							["ot"] = { "order_by_type", nowait = false },
 						},
 					},
 				},
@@ -294,7 +295,10 @@ return {
 						require("oil").open(node.path)
 					end,
 				},
-			})
+			}
+
+			require("neo-tree").setup(neotree_config)
+
 			vim.diagnostic.config({
 				signs = {
 					text = {
