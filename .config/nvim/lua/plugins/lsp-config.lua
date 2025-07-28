3432b76a39cbe2b230b281ee505fe7a8ae263cda
John Nguyen <ipwnponies@gmail.com>
2025-07-28 15:13:07 -0700

    [nvim] Replace neodev with lazydev.nvim
    Replaced deprecated plugin. Since we are nvim v0.10+

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -27,11 +27,21 @@ return {
 		end,
 	},
 	{
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+	{
 		"neovim/nvim-lspconfig",
 		event = { "BufReadPre", "BufNewFile" },
 		dependencies = {
 			"hrsh7th/cmp-nvim-lsp",
-			{ "folke/neodev.nvim", opts = {} }, -- deprecated for lazydev
 		},
 		config = function()
 			-- import lspconfig plugin
