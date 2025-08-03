297874f6133f9634543d464a4fb97b03441f8105
John Nguyen <ipwnponies@gmail.com>
2025-08-02 22:25:53 -0700

    [nvim] Remove nvim-web-devicons for mini.icons

--- .config/nvim/lua/plugins/file-explorer.lua
+++ .config/nvim/lua/plugins/file-explorer.lua
@@ -6,7 +6,7 @@ return {
 		branch = "v3.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
+			"echasnovski/mini.icons",
 			"MunifTanjim/nui.nvim",
 			"stevearc/oil.nvim",
 			{
@@ -72,21 +72,28 @@ return {
 						expander_expanded = "",
 						expander_highlight = "NeoTreeExpander",
 					},
+					---@type neotree.Component.Common.Icon
 					icon = {
 						folder_closed = "",
 						folder_open = "",
 						folder_empty = "󰜌",
-						provider = function(icon, node, state) -- default icon provider utilizes nvim-web-devicons if available
-							if node.type == "file" or node.type == "terminal" then
-								local success, web_devicons = pcall(require, "nvim-web-devicons")
-								local name = node.type == "terminal" and "terminal" or node.name
-								if success then
-									local devicon, hl = web_devicons.get_icon(name)
-									icon.text = devicon or icon.text
-									icon.highlight = hl or icon.highlight
+						folder_empty_open = "󰜌",
+						provider = function(icon, node)
+							local text, hl
+
+							if node.type == "file" or node.type == "directory" then
+								local mini_icons = require("mini.icons")
+								text, hl = mini_icons.get(node.type, node.name)
+
+								if node:is_expanded() then
+									text = nil
 								end
 							end
+
+							icon.highlight = hl
+							icon.text = text or icon.text
 						end,
+
 						-- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
 						-- then these will never be used.
 						default = "*",
