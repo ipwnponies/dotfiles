24d18c07b428cdbfc3532490ad30ba3b177ccc73
John Nguyen <ipwnponies@gmail.com>
2025-08-09 17:36:12 -0700

    [nvim] Add fzf-lua resume support
    All fzf commands are resume. This stores the last used query for that
    command.

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -4,7 +4,15 @@ return {
 	{
 		"ibhagwan/fzf-lua",
 		dependencies = { "echasnovski/mini.icons" },
-		opts = {},
+		opts = {
+			keymap = {
+				fzf = {
+					["ctrl-a"] = "toggle-all",
+					["home"] = "first",
+					["end"] = "last",
+				},
+			},
+		},
 		keys = {
 			{
 				"<leader>ff",
@@ -23,28 +31,28 @@ return {
 			{
 				"<leader>fb",
 				function()
-					require("fzf-lua").buffers({ resume = true, fzf_opts = { ["--query"] = "taco" } })
+					require("fzf-lua").buffers({ resume = true })
 				end,
 				desc = "Buffers",
 			},
 			{
 				"<leader>fG",
 				function()
-					require("fzf-lua").git_status()
+					require("fzf-lua").git_status({ resume = true })
 				end,
 				desc = "Git Status",
 			},
 			{
 				"<leader>fg",
 				function()
-					require("fzf-lua").git_files()
+					require("fzf-lua").git_files({ resume = true })
 				end,
 				desc = "Git Files",
 			},
 			{
 				"<leader>fl",
 				function()
-					require("fzf-lua").lines()
+					require("fzf-lua").lines({ resume = true })
 				end,
 				desc = "Lines across buffers",
 			},
@@ -52,7 +60,7 @@ return {
 			{
 				"<leader>/",
 				function()
-					require("fzf-lua").blines()
+					require("fzf-lua").blines({ resume = true })
 				end,
 				desc = "Buffer Lines",
 			},
@@ -60,7 +68,7 @@ return {
 			{
 				"<leader>fh",
 				function()
-					require("fzf-lua").help_tags()
+					require("fzf-lua").help_tags({ resume = true })
 				end,
 				desc = "Helptags",
 			},
@@ -68,6 +76,8 @@ return {
 		config = function(_, opts)
 			require("fzf-lua").setup(opts)
 			vim.opt.shell = "sh"
+			-- Disable FZF_DEFAULT_OPTS to avoid conflicts with fzf-lua
+			vim.env.FZF_DEFAULT_OPTS = ""
 		end,
 	},
 }
