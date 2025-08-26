b9ab7cee13b504f12527584b81279eb9cf5ebf28
John Nguyen <ipwnponies@gmail.com>
2025-08-25 18:11:06 -0700

    [nvim] Deduplicate highlight settings
    I knew I wasn't creative enough to come up wiht my own colour scheme.
    These are tweaks to sonokai. Move them closer, to avoid clobbering.

--- .config/nvim/lua/plugins/pretty.lua
+++ .config/nvim/lua/plugins/pretty.lua
@@ -11,6 +11,9 @@ return {
 		config = function()
 			vim.g.sonokai_style = "shusia"
 			vim.cmd([[colorscheme sonokai]])
+
+			vim.api.nvim_set_hl(0, "Visual", { bg = "#535D7E" }) -- Visual mode: muted blue-gray bg for subtle but visible selection
+			vim.api.nvim_set_hl(0, "Search", { bg = "#53575c" }) -- Search: muted green-gray to differentiate from Visual, still low contrast
 		end,
 	},
 }
