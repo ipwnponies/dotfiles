8a6e6f22c76102e54e27abc4c267efb3dc8d31b7
John Nguyen <ipwnponies@gmail.com>
2025-09-06 22:10:38 -0700

    [nvim] Fully deprecate vimcript configs
    Yay, we are now lua.
    And also 100% neovim, not at all pretending nor attempt to consider vim
    compatibility.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,10 +4,6 @@ require("keymaps") -- key mappings
 require("config.lazy") -- plugin manager and plugins
 require("binary-editing") -- binary file editing utilities
 require("autocmds")
-
-local xdg_config_home = os.getenv("XDG_CONFIG_HOME") or "~/.config"
--- Fallthrough to loading original vimscript
-vim.cmd("source " .. xdg_config_home .. "/nvim/init.vim")
 require("vim_settings") -- converted settings from init.vim
 require("colours") -- converted settings from init.vim
 
