b122fe57b46f118539466f82428530adebf0dbe8
John Nguyen <ipwnponies@gmail.com>
2025-09-19 00:35:54 -0700

    [fish] Add pyenv init setup
    `pyenv init` is meant to be dynamically added to shell startup.
    But... this slows down new shells. So strip it down, for 🏎️🏍️

--- .config/fish/conf.d/20-virtualenv.fish
+++ .config/fish/conf.d/20-virtualenv.fish
@@ -1,5 +1,51 @@
 # Installation of local virtualenv
 
+# The pyenv instructions use `pyenv init - |  source`. While convenient, it's poorly optimized:
+# - it spins up subshell to evaluate. Which is mostly static
+# - loads shell completions, via source. Not needed if you set up paths correctly
+# By inlining it here, we reduce costs by 30% or 100 ms
+function pyenv-init --description 'Stripped down, lightweight pyenv init'
+
+    # pyenv init -
+    while set pyenv_index (contains -i -- "/Users/jnguyen/.pyenv/shims" $PATH)
+        set -eg PATH[$pyenv_index]
+    end
+    set -gx PATH '/Users/jnguyen/.pyenv/shims' $PATH
+    set -gx PYENV_SHELL fish
+    command pyenv rehash 2>/dev/null
+
+    # Auto-activates pyenv virtualenvs
+    # pyenv shim magic is only for resolving to a versioned commands; it does not actually set up a virtualenv for LSPs that crave it
+    # pyenv virtualenv-init -
+    while set index (contains -i -- "/Users/jnguyen/.pyenv/plugins/pyenv-virtualenv/shims" $PATH)
+        set -eg PATH[$index]
+    end
+    set -gx PATH '/Users/jnguyen/.pyenv/plugins/pyenv-virtualenv/shims' $PATH;
+    set -gx PYENV_VIRTUALENV_INIT 1;
+
+    function pyenv
+        set command $argv[1]
+        set -e argv[1]
+
+        switch "$command"
+            case activate deactivate rehash shell
+                source (pyenv "sh-$command" $argv|psub)
+            case "*"
+                command pyenv "$command" $argv
+        end
+    end
+
+    function _pyenv_virtualenv_hook --on-event fish_prompt;
+        set -l ret $status
+        if [ -n "$VIRTUAL_ENV" ]
+            pyenv activate --quiet; or pyenv deactivate --quiet; or true
+        else
+            pyenv activate --quiet; or true
+        end
+        return $ret
+    end
+end
+
 function main
 end
 
@@ -25,6 +71,7 @@ function install
     set pyenv_virtualenv_plugin (pyenv root)/plugins/pyenv-virtualenv
     test -d $pyenv_virtualenv_plugin; or git clone https://github.com/pyenv/pyenv-virtualenv.git $pyenv_virtualenv_plugin
 
+    pyenv-init
 end
 
 status --is-login; and install
