dd1690d09727c86a46d2a6f3d1f4945ab77c3b6f
John Nguyen <ipwnponies@gmail.com>
2025-10-31 23:39:17 -0700

    [nvim] Add copilot chat completion source
    Adds all preset functions and makes the buffer one powerful.
    CopilotChat comes with completion but it is clobbering with blink.cmp

---
+++ .config/nvim/lua/completions/copilotchat_functions.lua
@@ -0,0 +1,87 @@
+local source = {}
+
+--- @module 'blink.cmp'
+--- @class blink.cmp.Source
+function source:new(opts)
+	local self = setmetatable({}, { __index = source })
+	self.opts = opts
+	return self
+end
+
+-- This source is only for CopilotChat
+function source:enabled()
+	return vim.bo.filetype == "copilot-chat"
+end
+
+function source:get_trigger_characters()
+	return { "#" }
+end
+
+local function add_open_buffer()
+	local bufs = vim.api.nvim_list_bufs()
+	local i = 0
+	return function()
+		while true do
+			i = i + 1
+			if i > #bufs then
+				return nil
+			end
+
+			local bufnr = bufs[i]
+
+			if vim.api.nvim_get_option_value("buflisted", { buf = bufnr }) then
+				local bufname = vim.api.nvim_buf_get_name(bufnr)
+				local relpath = vim.split(vim.fn.fnamemodify(bufname, ":."), "/")
+				local display_path = (relpath[#relpath - 1] or "") .. "/" .. relpath[#relpath]
+				return {
+					label = "#buffer:" .. display_path,
+					insertText = "#buffer:" .. bufnr,
+					priority = 1,
+				}
+			end
+		end
+	end
+end
+
+function source:get_completions(ctx, callback)
+	--- @type lsp.CompletionItem[]
+	local items = {}
+
+	local copilotchat_functions = {
+		"#buffer",
+		"#buffers:visible",
+		"#diagnostics:current",
+		"#file:",
+		"#gitdiff:staged",
+		"#gitstatus",
+		"#glob:**/*.lua",
+		"#grep:TODO",
+		"#quickfix",
+		"#register:+",
+		"#selection",
+		"#url:",
+	}
+
+	for _, func in ipairs(copilotchat_functions) do
+		--- @type lsp.CompletionItem
+		local item = {
+			label = func,
+			kind = require("blink.cmp.types").CompletionItemKind.Operator,
+			documentation = "I love eaeting taco... TBD",
+			insertTextFormat = vim.lsp.protocol.InsertTextFormat.PlainText,
+		}
+		table.insert(items, item)
+	end
+
+	for buf in add_open_buffer() do
+		table.insert(items, buf)
+	end
+
+	callback({
+		items = items,
+		is_incomplete_backward = false,
+		is_incomplete_forward = false,
+	})
+end
+
+return source
