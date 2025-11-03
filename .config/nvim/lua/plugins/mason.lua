2c62e68d76ecab1f09ec272452f8ca944430bbe5
John Nguyen <ipwnponies@gmail.com>
2025-11-03 09:33:17 -0800

    [nvim] Refactor lsp keymaps to separate function
    Just moving line around for reading.

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -32,6 +32,58 @@ for t, _ in pairs(ft) do
 	table.insert(ft_array, t)
 end
 
+local function setup_lsp_keymaps(bufnr)
+	local nmap = function(opts)
+		local keys = opts.keys
+		local func = opts.func
+		local desc = opts.desc and ("LSP: " .. opts.desc) or nil
+		local mode = opts.mode or "n"
+		vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = desc })
+	end
+
+	nmap({ keys = "<leader>rn", func = vim.lsp.buf.rename, desc = "[R]e[n]ame" })
+	nmap({
+		keys = "<leader>ca",
+		func = vim.lsp.buf.code_action,
+		desc = "[C]ode [A]ction",
+		mode = { "n", "v" },
+	})
+	nmap({ keys = "gd", func = vim.lsp.buf.definition, desc = "[G]oto [D]efinition" })
+	nmap({ keys = "gD", func = vim.lsp.buf.declaration, desc = "[G]oto [D]eclaration" })
+	nmap({
+		keys = "gr",
+		func = function()
+			vim.cmd("References")
+		end,
+		desc = "[G]oto [R]eferences",
+	})
+	nmap({ keys = "gR", func = "<cmd>Telescope lsp_references<CR>", desc = "Show LSP [R]eferences" })
+	nmap({ keys = "gI", func = vim.lsp.buf.implementation, desc = "[G]oto [I]mplementation" })
+	nmap({
+		keys = "gt",
+		func = "<cmd>Telescope lsp_type_definitions<CR>",
+		desc = "[G]oto [T]ype Definitions",
+	})
+	nmap({
+		keys = "<leader>D",
+		func = "<cmd>Telescope diagnostics bufnr=0<CR>",
+		desc = "Show Buffer [D]iagnostics",
+	})
+	nmap({ keys = "<leader>d", func = vim.diagnostic.open_float, desc = "Show Line [D]iagnostics" })
+	nmap({
+		keys = "<leader>ds",
+		func = require("telescope.builtin").lsp_document_symbols,
+		desc = "[D]ocument [S]ymbols",
+	})
+	nmap({
+		keys = "<leader>ws",
+		func = require("telescope.builtin").lsp_dynamic_workspace_symbols,
+		desc = "[W]orkspace [S]ymbols",
+	})
+	nmap({ keys = "K", func = vim.lsp.buf.hover, desc = "Hover Documentation" })
+	nmap({ keys = "<leader>k", func = vim.lsp.buf.signature_help, desc = "Signature Documentation" })
+end
+
 ---@type LazyPluginSpec | LazyPluginSpec[]
 return {
 	{
@@ -64,69 +116,6 @@ return {
 			local mason_lspconfig = require("mason-lspconfig")
 			mason_lspconfig.setup(opts)
 
-			local mason_lspconfig_on_attach = function(client, bufnr)
-				local nmap = function(opts)
-					opts = opts or {}
-					local keys = opts.keys
-					local func = opts.func
-					local desc = opts.desc
-					local mode = opts.mode or "n"
-					if desc then
-						desc = "LSP: " .. desc
-					end
-
-					vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = desc })
-				end
-				nmap({ keys = "<leader>rn", func = vim.lsp.buf.rename, desc = "[R]e[n]ame" })
-				nmap({
-					keys = "<leader>ca",
-					func = vim.lsp.buf.code_action,
-					desc = "[C]ode [A]ction",
-					mode = { "n", "v" },
-				})
-				nmap({ keys = "gd", func = vim.lsp.buf.definition, desc = "[G]oto [D]efinition" })
-				nmap({ keys = "gD", func = vim.lsp.buf.declaration, desc = "[G]oto [D]eclaration" })
-				nmap({
-					keys = "gr",
-					func = function()
-						vim.cmd("References")
-					end,
-					desc = "[G]oto [R]eferences",
-				})
-				nmap({ keys = "gR", func = "<cmd>Telescope lsp_references<CR>", desc = "Show LSP [R]eferences" })
-				nmap({ keys = "gI", func = vim.lsp.buf.implementation, desc = "[G]oto [I]mplementation" })
-				nmap({
-					keys = "gt",
-					func = "<cmd>Telescope lsp_type_definitions<CR>",
-					desc = "[G]oto [T]ype Definitions",
-				})
-
-				nmap({
-					keys = "<leader>D",
-					func = "<cmd>Telescope diagnostics bufnr=0<CR>",
-					desc = "Show Buffer [D]iagnostics",
-				})
-				nmap({ keys = "<leader>d", func = vim.diagnostic.open_float, desc = "Show Line [D]iagnostics" })
-
-				nmap({
-					keys = "<leader>ds",
-					func = require("telescope.builtin").lsp_document_symbols,
-					desc = "[D]ocument [S]ymbols",
-				})
-				nmap({
-					keys = "<leader>ws",
-					func = require("telescope.builtin").lsp_dynamic_workspace_symbols,
-					desc = "[W]orkspace [S]ymbols",
-				})
-				nmap({ keys = "K", func = vim.lsp.buf.hover, desc = "Hover Documentation" })
-				nmap({ keys = "<leader>k", func = vim.lsp.buf.signature_help, desc = "Signature Documentation" })
-
-				vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-					vim.lsp.buf.format()
-				end, { desc = "Format current buffer with LSP" })
-			end
-
-			-- used to enable autocompletion (assign to every lsp server config)
 			local capabilities = require("lazy.core.config").plugins["blink.cmp"]
 					and require("blink.cmp").get_lsp_capabilities({
 						textDocument = { completion = { completionItem = { snippetSupport = false } } },
@@ -140,24 +129,28 @@ return {
 				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 			end
 
+			local function on_attach(_, bufnr)
+				setup_lsp_keymaps(bufnr)
+
+				vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+					vim.lsp.buf.format()
+				end, { desc = "Format current buffer with LSP" })
+			end
+
 			local lspconfig = require("lspconfig")
 			mason_lspconfig.setup_handlers({
-				-- default handler for installed servers
 				function(server_name)
 					vim.lsp.config(server_name, {
 						capabilities = capabilities,
-						on_attach = mason_lspconfig_on_attach,
+						on_attach = on_attach,
 					})
 					vim.lsp.enable(server_name)
 				end,
 				["pyright"] = function()
 					vim.lsp.config("pyright", {
 						capabilities = capabilities,
-						on_attach = mason_lspconfig_on_attach,
+						on_attach = on_attach,
 						root_dir = function(fname)
-							---@type fun(fname: string): table|nil
-							vim.g.project_pyright_root = vim.g.project_pyright_root
-
 							if vim.g.project_pyright_root then
 								local patterns = vim.g.project_pyright_root(fname)
 								if patterns ~= nil then
