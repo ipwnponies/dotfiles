19f7ba4f50a7280c5e87869d2b72085fa910eb4d
John Nguyen <ipwnponies@gmail.com>
2025-11-06 18:09:45 -0800

    [nvim] Remove copilot autocommands
    We don't use this, completions through blink.cmp only.

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -207,21 +207,8 @@ return {
 				help = true,
 			},
 		},
-		config = function()
-			require("copilot").setup({})
-			vim.api.nvim_create_autocmd("User", {
-				pattern = "BlinkCmpMenuOpen",
-				callback = function()
-					vim.b.copilot_suggestion_hidden = true
-				end,
-			})
-
-			vim.api.nvim_create_autocmd("User", {
-				pattern = "BlinkCmpMenuClose",
-				callback = function()
-					vim.b.copilot_suggestion_hidden = false
-				end,
-			})
+		config = function(_, opts)
+			require("copilot").setup(opts)
 		end,
 	},
 	{
