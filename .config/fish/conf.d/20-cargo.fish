844bf9bc5fa1c7e64c1cab78a2f84fda1a16291d
John Nguyen <ipwnponies@gmail.com>
2025-12-09 19:23:40 -0800

    [cargo] Install treesitter-cli

--- .config/fish/conf.d/20-cargo.fish
+++ .config/fish/conf.d/20-cargo.fish
@@ -4,6 +4,7 @@ fish_add_path $CARGO_INSTALL_ROOT/bin --append
 
 function main
     not type --query _pay-respects-module-100-runtime-rules; and cargo install pay-respects-module-runtime-rules
+    cargo install --locked tree-sitter-cli
 end
 
 status --is-login; and main
