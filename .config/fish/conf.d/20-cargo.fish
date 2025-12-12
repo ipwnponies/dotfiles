fd67ac3a6fe62ab52588343ae0d4b495fe90b10a
John Nguyen <ipwnponies@gmail.com>
2025-12-12 11:23:32 -0800

    [fish] Move cargo spec to separate file
    Move cargo crates to a separate file.
    Also fix the version specifier to use caret notation. Cargo build treats
    1.1.1 as semver but install treats it as pinned value. We want loosely
    pinned value.

--- .config/fish/conf.d/20-cargo.fish
+++ .config/fish/conf.d/20-cargo.fish
@@ -3,7 +3,7 @@ set -x CARGO_TARGET_DIR $XDG_CACHE_HOME/cargo
 fish_add_path $CARGO_INSTALL_ROOT/bin --append
 
 function main
-    set crates pay-respects-module-runtime-rules@0.1.9 tree-sitter-cli@0.26.2
+    set crates (cat $XDG_CONFIG_HOME/cargo/tools.txt)
     for i in $crates
         cargo install --quiet --locked $i
     end
