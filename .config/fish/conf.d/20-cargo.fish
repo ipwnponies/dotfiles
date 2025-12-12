53ad6a534548d71ff7c8c2c89460bcdda213bf63
John Nguyen <ipwnponies@gmail.com>
2025-12-11 23:08:22 -0800

    [fish] Install cargo crates in a declarative manner
    Use --locked, so that transitive dependencies are fixed.
    Specify the minimum version targeted, default rust version is semver.

--- .config/fish/conf.d/20-cargo.fish
+++ .config/fish/conf.d/20-cargo.fish
@@ -3,8 +3,10 @@ set -x CARGO_TARGET_DIR $XDG_CACHE_HOME/cargo
 fish_add_path $CARGO_INSTALL_ROOT/bin --append
 
 function main
-    not type --query _pay-respects-module-100-runtime-rules; and cargo install pay-respects-module-runtime-rules
-    cargo install --locked tree-sitter-cli
+    set crates pay-respects-module-runtime-rules@0.1.9 tree-sitter-cli@0.26.2
+    for i in $crates
+        cargo install --quiet --locked $i
+    end
 end
 
 status --is-login; and main
