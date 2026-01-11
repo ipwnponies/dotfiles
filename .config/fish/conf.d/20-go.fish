0d73ef24d53e2e087dc60974a43cc060595f0979
John Nguyen <ipwnponies@gmail.com>
2026-01-10 23:10:29 -0800

    [fish] Add aqua
    Aqua is a go package manager

--- .config/fish/conf.d/20-go.fish
+++ .config/fish/conf.d/20-go.fish
@@ -1,5 +1,13 @@
 set -gx GOPATH $XDG_DATA_HOME/go
 fish_add_path --global $GOPATH/bin
 
-if status --is-login; and status --is-interactive; and type -q go
+set AQUA_GLOBAL_CONFIG $XDG_CONFIG_HOME/aqua/aqua.yaml
+
+function install_aqua --description 'Install aqua if not present'
+    go install github.com/aquaproj/aqua/v2/cmd/aqua@latest
 end
+
+type -q aqua; or install_aqua
+status --is-login; and aqua install
+
+fish_add_path (aqua root-dir)/bin
