127062c658fd18e3d882eaef04f6719d342210cf
John Nguyen <ipwnponies@gmail.com>
2026-01-24 17:35:14 -0800

    [fish] Remove sort from fzf default args
    --sort is the default behaviour. omf-plugin-expand uses FZF_DEFAULT_OPTS
    and no way to override. So remove sorting, such that input ordering can
    be used by default.
    It can toggled on with shortcut, so this is about choosing the more
    common use case as the default.
    
    Input order often carries semantic information.
    Lexical sorting is more for convenience and aesthetic. If we desire
    this, can achieve by piping input to `sort` or pre-sorting the input via
    processing before passing to fzf.

--- .config/fish/conf.d/fzf.fish
+++ .config/fish/conf.d/fzf.fish
@@ -1,7 +1,9 @@
 if status --is-interactive; and type -q fzf
-    set -gx FZF_DEFAULT_OPTS "--reverse --select-1 --exit-0 \
+    set -gx FZF_DEFAULT_OPTS "--reverse --select-1 --no-sort --exit-0 \
     --border double --height 40% --min-height 10 --margin 1,5 --info inline \
     --bind pgdn:preview-page-down,pgup:preview-page-up,change:top,ctrl-s:toggle-sort,ctrl-a:toggle-all"
+
+    set -gx FZF_CTRL_R_OPTS --sort
 
     fzf --fish | source
 end
