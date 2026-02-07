31d352f3debf02256be19d289210ffb26f34b25a
John Nguyen <ipwnponies@gmail.com>
2026-02-06 17:43:01 -0800

    [nvim] Replace indentline with modern replacement
    indent-blankline is a neovim lua native plugin. This means it is
    treesitter-aware and uses the shadow text feature. Instead of hacky
    charcter rewriting that was necessary in older vim. Which means it's
    likely to be more robust and featureful.
    Also the indentLine is not longer supported.

--- .config/nvim/lua/plugins/indent-line.lua
+++ .config/nvim/lua/plugins/indent-line.lua
@@ -1,15 +1,10 @@
 ---@type LazyPluginSpec | LazyPluginSpec[]
 return {
 	{
-		"Yggdroot/indentLine",
-		init = function()
-			vim.g.indentLine_fileTypeExclude = { "markdown", "json" }
-		end,
-		config = function()
-			-- Set conceal cursor to always show
-			-- Need to set indentline plugin because it rudely clobbers concealcursor for reasons
-			vim.opt.concealcursor = "c" -- Disable conceal during insert and visual mode; only in normal and command-line modes
-			vim.g.indentLine_concealcursor = vim.o.concealcursor
-		end,
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		---@module "ibl"
+		---@type ibl.config
+		opts = {},
 	},
 }
