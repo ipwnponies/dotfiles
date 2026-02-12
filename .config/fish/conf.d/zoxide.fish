496fe3e2509c156179e866b21631accfcf2d73d9
John Nguyen <ipwnponies@gmail.com>
2026-02-11 20:55:50 -0800

    [fish] Add fzf sorting to zoxide
    Add fzf sorting back to zoxide. Previously in 127062c, we removed sort
    from FZF default options.
    Sorting is quite useful for zoxide, since the user knows where they want
    to go and recency is a valaable metric. Zoxide scoring already accounts
    for recency.

--- .config/fish/conf.d/zoxide.fish
+++ .config/fish/conf.d/zoxide.fish
@@ -1,11 +1,11 @@
 if status --is-interactive
-    set --export _ZO_FZF_OPTS "$FZF_DEFAULT_OPTS --keep-right --scheme path"
+    set --export _ZO_FZF_OPTS "$FZF_DEFAULT_OPTS --keep-right --scheme path --sort"
 
     zoxide init fish --cmd cd | source
 
     # fzf-widget calls `cd -- $argv` and zoxide doesn't understand '--'
     function cd --wraps=__zoxide_z --description 'Override default zoxide cd alias'
-        if test (count $argv) -gt 0; and test $argv[1] = '--'
+        if test (count $argv) -gt 0; and test $argv[1] = --
             set --erase argv[1]
         end
         __zoxide_z $argv
