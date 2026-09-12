271cedb7dac2ef270305094d5e118d989c227fb0
John Nguyen <ipwnponies@gmail.com>
2026-09-11 18:57:35 -0700

    feat(fish): Improve z-wait script
    This is fish conf.d script that blocks interactively while other scripts
    are still running. It will print details.
    If disabled, the shell is immediately available to user and nothing is
    displayed.

--- .config/fish/conf.d/z-wait.fish
+++ .config/fish/conf.d/z-wait.fish
@@ -1,4 +1,11 @@
 if status --is-interactive; and set -q conf_wait
-    echo Waiting patiently for all scripts to complete...
+    set start (date +%s)
+    set njobs (count (jobs -p))
+    set jobs_desc (string join ', ' (jobs --command))
+    printf '%s' (set_color brblue)'⏳ Waiting for '$njobs' job(s): '$jobs_desc(set_color normal)
+
     wait
+
+    set elapsed (math (date +%s) - $start)
+    printf '\r\033[K%s\n' (set_color brgreen)'✓ Done in '$elapsed's: '$njobs' job(s): '$jobs_desc(set_color normal)
 end
